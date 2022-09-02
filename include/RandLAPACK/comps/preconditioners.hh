10429da203eac1936c4fd1fabff6ad4ba6c0089f
rileyjmurray <rjmurray@berkeley.edu>
2022-09-02 11:46:32 -0700

    left out of last commit

---
+++ include/RandLAPACK/comps/preconditioners.hh
@@ -0,0 +1,22 @@
+#ifndef BLAS_HH
+#include <blas.hh>
+#define BLAS_HH
+#endif
+
+namespace RandLAPACK::comps::preconditioners {
+
+template <typename T>
+void rpc_svd_sjlt(
+    int64_t m, // number of rows in A_rm
+    int64_t n, // number of columns in A_rm
+    int64_t d, // number of rows in sketch of A_rm
+    int64_t k, // number of nonzeros in each column of the sketching operator
+    std::vector<T> &A_rm, // row major
+    std::vector<T> &M_wk, // length at least d*n; 
+    T mu,
+    int64_t threads, // number of OpenMP threads to use in sketching.
+    uint64_t seed_key,
+    uint64_t seed_ctr
+);
+
+}
