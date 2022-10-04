c26fc778a79987adbc765a6835638bef2e450e02
Burlen Loring <bloring@lbl.gov>
2022-10-04 10:11:57 -0700

    improve semantic versioning

--- CMake/version.cmake
+++ CMake/version.cmake
@@ -2,13 +2,15 @@ set(tmp)
 find_package(Git QUIET)
 if(GIT_FOUND)
     execute_process(COMMAND ${GIT_EXECUTABLE}
-        --git-dir=${CMAKE_SOURCE_DIR}/.git describe --tags
+        --git-dir=${CMAKE_SOURCE_DIR}/.git describe
+        --tags --match "[0-9]*.[0-9]*.[0-9]*"
         OUTPUT_VARIABLE tmp OUTPUT_STRIP_TRAILING_WHITESPACE
         ERROR_QUIET)
 endif()
 if(NOT tmp)
     set(tmp "0.0.0")
 endif()
+
 set(RandLAPACK_VERSION ${tmp} CACHE STRING "RandLAPACK version" FORCE)
 
 string(REGEX REPLACE "^([0-9]+)\\.([0-9]+)\\.([0-9]+)(.*$)"
