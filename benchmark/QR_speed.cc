6786c56b236294b576c510cc3e8f87869e223d5a
Melnichenko Max <thrcn@TeachRaccoon>
2022-10-27 23:22:56 -0400

    Added ChoQRCP benchmarks

---
+++ benchmark/QR_speed.cc
@@ -0,0 +1,169 @@
+/*
+TODO #1: Switch tuples to vectors.
+*/
+
+#include <blas.hh>
+#include <RandBLAS.hh>
+#include <lapack.hh>
+#include <RandLAPACK.hh>
+#include <math.h>
+
+#include <numeric>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
+using namespace std::chrono;
+
+#include <fstream>
+
+#define RELDTOL 1e-10;
+#define ABSDTOL 1e-12;
+
+using namespace RandLAPACK::comps::util;
+using namespace RandLAPACK::comps::cholqrcp;
+
+template <typename T>
+static std::vector<long> 
+test_speed_helper(int64_t m, int64_t n, uint32_t seed) {
+
+    using namespace blas;
+    using namespace lapack;
+
+    int64_t size = m * n;
+    std::vector<T> A_1(size, 0.0);
+    std::vector<T> A_2(size, 0.0);
+    std::vector<T> A_3(size, 0.0);
+    
+    std::vector<T> Q_1;
+    std::vector<T> R_1;
+    std::vector<int64_t> J_1(n, 0);
+
+    std::vector<int64_t> J_2(n, 0.0);
+    std::vector<int64_t> tau_2(n, 0);
+
+    std::vector<int64_t> t_3(5, 0);
+    std::vector<int64_t> tau_3(n, 0);
+    std::vector<int64_t> J_3(n, 0);
+
+    // Random Gaussian test matrix
+    RandBLAS::dense_op::gen_rmat_norm<T>(m, n, A_dat, seed);
+    // Make a copy
+    std::copy(A_dat, A_dat + size, A_cpy_dat);
+    std::copy(A_dat, A_dat + size, A_cpy_2_dat);
+    std::copy(A_dat, A_dat + size, A_cpy_3_dat);
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+
+    // CholQRCP constructor
+    Stab<T> CholQRCP(false, seed, 1.0e-16, use_cholqrcp1);
+
+    // CholQRCP
+    auto start_cholqrcp = high_resolution_clock::now();
+    CholQRCP.call(m, n, A_1, d, Q_1, R_1, J_1);
+    auto stop_cholqrcp = high_resolution_clock::now();
+    long dur_cholqrcp = duration_cast<microseconds>(stop_cholqrcp - start_cholqrcp).count();
+
+    // GEQP3
+    auto start_geqp3 = high_resolution_clock::now();
+    geqp3(m, n, A_2.data(), m, J_2.data(), tau_2.data());
+    auto stop_geqp3 = high_resolution_clock::now();
+    long dur_geqp3 = duration_cast<microseconds>(stop_geqp3 - start_geqp3).count();
+
+    // TSQR + GEQP3
+    auto start_tsqrp = high_resolution_clock::now();
+    // TSQR part
+    geqr(m, n, A_3.data(), m, t_3.data(), -1);
+    int64_t tsize = (int64_t) t_3[0]; 
+    t_3.resize(tsize);
+    geqr(m, n, A_3.data(), m, t_3.data(), tsize)
+
+    // GEQP3 on R part
+    // Get_U(m, n, A_3);
+    // geqp3(m, n, A_3.data(), m, J_3.data(), tau_3.data());
+
+    // We then want to combine the representations of GEQP3 and GEQR
+
+    auto stop_tsqrp = high_resolution_clock::now();
+    long dur_tsqrp = duration_cast<microseconds>(stop_tsqrp - start_tsqrp).count();
+
+    std::vector<long> res{dur_cholqrcp, dur_geqp3, dur_tsqrp}; 
+    return res;
+}
+
+
+template <typename T>
+static void 
+test_speed_mean(int r_pow, int r_pow_max, int col, int col_max, int runs)
+{
+
+    // Clear all files
+    for(int r_buf = r_pow; r_buf <= r_pow_max; ++r_buf)
+    {
+        int rows = std::pow(2, r_buf);
+        std::ofstream ofs;
+        ofs.open("../../../test_plots/test_speed_full_Q/raw_data/test_mean_time_" + std::to_string(rows) + ".dat", std::ofstream::out | std::ofstream::trunc);
+        ofs.close();
+    }
+
+    int64_t rows = 0;
+    int64_t cols = 0;
+
+    T cholqrcp_avg = 0;
+    T geqp3_avg    = 0;
+    T tsqrp_avg    = 0;
+
+    for(; r_pow <= r_pow_max; ++r_pow)
+    {
+        rows = std::pow(2, r_pow);
+        int64_t cols = col;
+
+        for (; cols <= col_max; cols += 64)
+        {
+            std::vector<long> res;
+            long t_cholqrcp = 0;
+            long t_geqp3    = 0;
+            long t_tsqrp    = 0;
+
+            long curr_t_cholqrcp = 0;
+            long curr_t_geqp3    = 0;
+            long curr_t_tsqrp    = 0;
+
+            for(int i = 0; i < runs; ++i)
+            {
+                res = test_speed_helper<T>(rows, cols, i);
+                curr_t_cholqrcp = res[1];
+                curr_t_geqp3    = res[0];
+                curr_t_tsqrp    = res[0];
+
+                // Skip first iteration, as it tends to produce garbage results
+                if (i != 0)
+                {
+                    t_cholqrcp += curr_t_cholqrcp;
+                    t_geqp3    += curr_t_geqp3;
+                    t_tsqrp    += curr_t_tsqrp;
+                }
+            }
+
+            cholqrcp_avg = (T)t_cholqrcp / (T)(runs - 1);
+            geqp3_avg    = (T)t_geqp3    / (T)(runs - 1);
+            tsqrp_avg    = (T)t_tsqrp    / (T)(runs - 1);
+
+            // Save the output into .dat file
+            std::fstream file("../../../test_plots/test_speed_full_Q/raw_data/test_mean_time_" + std::to_string(rows) + ".dat", std::fstream::app);
+            file << cholqrcp_avg << "  " << geqp3_avg << tsqrp_avg << "\n";
+
+            printf("\nMatrix size: %ld by %ld.\n", rows, cols);
+            printf("Average timing of CholQRCP for %d runs: %f μs.\n", runs - 1, cholqrcp_avg);
+            printf("Average timing of GEQP3 for %d runs: %f μs.\n", runs - 1, geqp3_avg);
+            printf("Average timing of TSQRP for %d runs: %f μs.\n", runs - 1, tsqrp_avg);
+            printf("Result: CholQRCP is %f times faster than GEQP3.\n\n", cholqrcp_avg / geqp3_avg);
+            printf("Result: CholQRCP is %f times faster than TSQRP.\n\n", cholqrcp_avg / tsqrp_avg);
+        }
+    }
+}
+
+int main(int argc, char **argv){
+    test_speed_mean<double>(12, 12, 64, 64, 3);
+    return 0;
+}
--- src/drivers/cholqrcp.cc
+++ src/drivers/cholqrcp.cc
@@ -99,15 +99,13 @@ int CholQRCP<T>::CholQRCP1(
     int64_t m,
     int64_t n,
     std::vector<T>& A,
-    int64_t& d,
+    int64_t d,
     std::vector<T>& Q,
     std::vector<T>& R,
     std::vector<int64_t>& J
 ){
-    // What to do about these?
-
-    T* Q_dat       = upsize(m * n, Q);
     T* A_dat       = A.data();
+    T* Q_dat       = upsize(m * n, Q);
     T* A_hat_dat   = upsize(d * n, this->A_hat);
     T* tau_dat     = upsize(n, this->tau);
     J.resize(n);
@@ -115,16 +113,12 @@ int CholQRCP<T>::CholQRCP1(
 
     copy<T, T>(m * n, A_dat, 1, Q_dat, 1);
     
-    // Generate a random matrix
-    //T* Omega_dat = upsize(d * m, this->Omega);
-    //RandBLAS::dense_op::gen_rmat_norm<T>(d, m, Omega_dat, this->seed);
-    //gemm<T>(Layout::ColMajor, Op::NoTrans, Op::NoTrans, d, n, m, 1.0, Omega_dat, d, Q_dat, m, 0.0, A_hat_dat, d);
-
+    // Need a wrapper for SJLT
     struct RandBLAS::sjlts::SJLT sjl;
     sjl.ori = RandBLAS::sjlts::ColumnWise;
     sjl.n_rows = d; // > n
     sjl.n_cols = m;
-    sjl.vec_nnz = 8; // <= n_rows
+    sjl.vec_nnz = 8; // Arbitrary constant, Riley likes 8s
     sjl.rows = new uint64_t[sjl.vec_nnz * m];
     sjl.cols = new uint64_t[sjl.vec_nnz * m];
     sjl.vals = new double[sjl.vec_nnz * m];
@@ -151,7 +145,6 @@ int CholQRCP<T>::CholQRCP1(
     T* R_sp_dat  = upsize(k * k, this->R_sp);
     T* R_dat     = upsize(k * n, R);
     T* R_buf_dat = upsize(k * n, this -> R_buf);
-    T* A_buf_dat = upsize(k * k, this->A_buf);
 
     // extract k by k R
     // Copy data over to R_sp_dat col by col
@@ -172,16 +165,91 @@ int CholQRCP<T>::CholQRCP1(
     trsm(Layout::ColMajor, Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, m, k, 1.0, R_sp_dat, k, Q_dat, m);
     
     // Do Cholesky QR
-    syrk(Layout::ColMajor, Uplo::Upper, Op::Trans, k, m, 1.0, Q_dat, m, 0.0, A_buf_dat, k);
-    potrf(Uplo::Upper, k, A_buf_dat, k);
-    trsm(Layout::ColMajor, Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, m, k, 1.0, A_buf_dat, k, Q_dat, m);
+    syrk(Layout::ColMajor, Uplo::Upper, Op::Trans, k, m, 1.0, Q_dat, m, 0.0, R_sp_dat, k);
+    potrf(Uplo::Upper, k, R_sp_dat, k);
+    trsm(Layout::ColMajor, Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, m, k, 1.0, R_sp_dat, k, Q_dat, m);
 
     // Get R
-    gemm(Layout::ColMajor, Op::NoTrans, Op::NoTrans, k, n, k, 1.0, A_buf_dat, k, R_buf_dat, k, 0.0, R_dat, k);
+    gemm(Layout::ColMajor, Op::NoTrans, Op::NoTrans, k, n, k, 1.0, R_sp_dat, k, R_buf_dat, k, 0.0, R_dat, k);
     
     return 0;
 }
 
-template int CholQRCP<float>::CholQRCP1(int64_t m, int64_t n, std::vector<float>& A, int64_t& d, std::vector<float>& Q, std::vector<float>& R, std::vector<int64_t>& J);
-template int CholQRCP<double>::CholQRCP1(int64_t m, int64_t n, std::vector<double>& A, int64_t& d, std::vector<double>& Q, std::vector<double>& R, std::vector<int64_t>& J);
+template <typename T>
+int CholQRCP<T>::CholQRCP2(
+    int64_t m,
+    int64_t n,
+    std::vector<T>& A,
+    int64_t d,
+    int64_t b_sz,
+    std::vector<T>& Q,
+    std::vector<T>& R,
+    std::vector<int64_t>& J
+){
+    /*
+    T* Q_dat       = upsize(m * n, Q);
+    T* A_dat       = A.data();
+    T* A_hat_dat   = upsize(d * n, this->A_hat);
+    T* tau_dat     = upsize(n, this->tau);
+    J.resize(n);
+    int64_t* J_dat = J.data();
+
+    copy<T, T>(m * n, A_dat, 1, Q_dat, 1);
+
+    for (int i = 0; i < n / b_sz; ++i)
+    {
+        // View to the row dimension of A_cpy, changes with every iteration
+        int_64_t sz = m;
+        
+        // Need a wrapper for SJLT
+        struct RandBLAS::sjlts::SJLT sjl;
+        sjl.ori = RandBLAS::sjlts::ColumnWise;
+        sjl.n_rows = d;
+        sjl.n_cols = sz;
+        sjl.vec_nnz = 8; // Arbitrary constant, Riley likes 8s
+        sjl.rows = new uint64_t[sjl.vec_nnz * sz];
+        sjl.cols = new uint64_t[sjl.vec_nnz * sz];
+        sjl.vals = new double[sjl.vec_nnz * sz];
+        RandBLAS::sjlts::fill_colwise(sjl, this->seed, 0);
+
+        // A_hat = S * A
+        RandBLAS::sjlts::sketch_csccol(sjl, sz, n, (double*) Q_dat, (double*) A_hat_dat);
+
+        geqp3(d, n, A_hat_dat, d, J_dat, tau_dat);
+
+        // Need to do something about the size of R
+        col_swap(m, n, k, R, J);
+        // A = AJ[:, b_sz]
+        col_swap(sz, n, n, Q, J); // need a copy of this
+        // Figure out what to do about the isze of this
+        copy<T, T>(sz * n, Q_dat, 1, Q_cpy_dat, 1);
+
+        // [Q_, R_11_full] = qr(AJ[:, b_sz])
+        geqrf(sz, b_sz, Q_cpy_dat, sz, this->tau_dat);
+
+        //R12 = Q_(:, 1:b_sz)' * A * J(:, b_sz + 1:end);
+
+
+        //A = Q_(:, b_sz + 1:end)' * A * J(:, b_sz + 1:end);
+
+        //QI = eye(m, m);
+        //PI = eye(n, n);
+
+        //QI((j - 1) * b_sz + 1: end, (j - 1) * b_sz + 1 : end) = Q_;
+        //PI((j - 1) * b_sz + 1: end, (j - 1) * b_sz + 1 : end) = J;
+
+        //Q = Q * QI;
+        //P = P * PI;
+        
+        //R1 = [R11 R12];
+
+        //R((j - 1) * b_sz + 1: j * b_sz, (j - 1) * b_sz + 1: end) = R1;  
+    }
+    */
+   
+    return 0;
+}
+
+template int CholQRCP<float>::CholQRCP1(int64_t m, int64_t n, std::vector<float>& A, int64_t d, std::vector<float>& Q, std::vector<float>& R, std::vector<int64_t>& J);
+template int CholQRCP<double>::CholQRCP1(int64_t m, int64_t n, std::vector<double>& A, int64_t d, std::vector<double>& Q, std::vector<double>& R, std::vector<int64_t>& J);
 }
