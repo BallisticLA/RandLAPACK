c4c77c6fe9681d7dcacf322343b117584b157463
Melnichenko Max <thrcn@TeachRaccoon>
2022-11-21 11:21:23 -0500

    Switch from SJLT to SASO

--- src/drivers/cholqrcp.cc
+++ src/drivers/cholqrcp.cc
@@ -253,17 +253,17 @@ int CholQRCP<T>::CholQRCP1(
         total_t_stop = high_resolution_clock::now();
         total_t_dur = duration_cast<microseconds>(total_t_stop - total_t_start).count();
 
-        printf("\n\n/**********CholQRCP1 TIMING RESULTS BEGIN**********/");
+        printf("\n\n/**********CholQRCP1 TIMING RESULTS BEGIN**********/\n");
         printf("SASO time: %d μs,\n", saso_t_dur);
         printf("QRCP time: %d μs,\n", qrcp_t_dur);
         printf("Rank revealing time: %d μs,\n", rank_reveal_t_dur);
         printf("CholQRCP time: %d μs,\n", cholqrcp_t_dur);
         printf("Total time: %d μs,\n", total_t_dur);
 
-        printf("SASO generation and application takes %f %% of runtime.\n", 100 * ((double) saso_t_dur / (double) total_t_dur));
-        printf("QRCP takes %f %% of runtime.\n", 100 * ((double) qrcp_t_dur / (double) total_t_dur));
-        printf("Rank revealing takes %f %% of runtime.\n", 100 * ((double) rank_reveal_t_dur / (double) total_t_dur));
-        printf("Cholqrcp takes %f %% of runtime.\n", 100 * ((double) cholqrcp_t_dur / (double) total_t_dur));
+        printf("\nSASO generation and application takes %.1f%% of runtime.\n", 100 * ((double) saso_t_dur / (double) total_t_dur));
+        printf("QRCP takes %.1f%% of runtime.\n", 100 * ((double) qrcp_t_dur / (double) total_t_dur));
+        printf("Rank revealing takes %.1f%% of runtime.\n", 100 * ((double) rank_reveal_t_dur / (double) total_t_dur));
+        printf("Cholqrcp takes %.1f%% of runtime.\n", 100 * ((double) cholqrcp_t_dur / (double) total_t_dur));
         printf("/*********CholQRCP1 TIMING RESULTS END*********/\n\n");
     }
     /*****TIMING******/
