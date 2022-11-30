7235f757d12d8e5f8a38e6c8ef189a30b524cd63
Melnichenko Max <thrcn@TeachRaccoon>
2022-11-30 16:44:20 -0500

    update

--- benchmark/QR_speed_basic.cc
+++ benchmark/QR_speed_basic.cc
@@ -193,7 +193,7 @@ test_speed_mean(int r_pow, int r_pow_max, int col, int col_max, int runs)
 int main(int argc, char **argv){
         
     //test_speed_mean<double>(14, 17, 64, 1024, 64, 10);
-    test_speed_mean<double>(17, 17, 1024, 131072, 5);
+    test_speed_mean<double>(16, 16, 512, 65536, 5);
     
     return 0;
 }
