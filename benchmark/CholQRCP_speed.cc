4af1599abeb6f3f86db53fa997cf37a9e082cd97
Melnichenko Max <thrcn@TeachRaccoon>
2022-12-12 16:40:20 -0500

    Update

--- benchmark/CholQRCP_speed.cc
+++ benchmark/CholQRCP_speed.cc
@@ -709,7 +709,9 @@ int main(int argc, char **argv){
 
     //test_speed<double>(16, 16, 256, 4096, 5, 1, 36, std::pow(1.0e-16, 0.75), 1.0, 1.0, std::make_tuple(6, 0, false)); 
 
-    test_speed<double>(17, 17, 512, 8192, 5, 1, 36, std::pow(1.0e-16, 0.75), 1.0, 1.0, std::make_tuple(6, 0, false));
+    //test_speed<double>(17, 17, 512, 8192, 5, 1, 36, std::pow(1.0e-16, 0.75), 1.0, 1.0, std::make_tuple(6, 0, false));
+
+    test_speed<double>(18, 18, 2048, 8192, 5, 1, 36, std::pow(1.0e-16, 0.75), 1.0, 1.0, std::make_tuple(6, 0, false));
 
     return 0;
 }
