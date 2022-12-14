6186b09dd539de9f225944d533ca5ce323d1bb27
Melnichenko Max <thrcn@TeachRaccoon>
2022-12-14 15:43:52 -0500

    Update

--- benchmark/CholQRCP_speed.cc
+++ benchmark/CholQRCP_speed.cc
@@ -573,7 +573,7 @@ test_speed(int r_pow,
                     t_alloc5   += res[12];
                     t_geqrf    += res[13];
                     t_appl5    += res[14];
-                    /*
+                    
                     // Log every run in the raw data file
                     std::fstream file("../../../testing/RandLAPACK-Testing/test_benchmark/QR/speed/raw_data/CholQRCP_comp_time_Raw_m_" 
                                                                                                                          + std::to_string(rows) 
@@ -601,7 +601,7 @@ test_speed(int r_pow,
                          << res[12] << "  "
                          << res[13] << "  "
                          << res[14] << "\n";
-                    */
+                    
                     // For best timing
                     if(alloc1_best > res[0] || alloc1_best == 0)
                     {
@@ -682,7 +682,7 @@ test_speed(int r_pow,
             alloc5_mean   = (T)t_alloc5   / (T)(runs - 1);
             geqrf_mean    = (T)t_geqrf    / (T)(runs - 1);
             appl5_mean    = (T)t_appl5    / (T)(runs - 1);
-            /*
+            
             log_info(rows, cols, d_multiplier, k_multiplier, tol, nnz, num_threads, mat_type, 
                      cholqrcp_best,
                      geqp3_best,
@@ -708,7 +708,6 @@ test_speed(int r_pow,
                      tsqrp_mean    + alloc4_mean + appl4_mean, 
                      geqrf_mean    + alloc5_mean + appl5_mean,
                      "Mean", runs);
-            */
         }
     }
     printf("\n/-----------------------------------------SPEED TEST STOP-----------------------------------------/\n\n");
