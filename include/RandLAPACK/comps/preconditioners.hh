f0528baf5032f7a483c5cd11e4eb339a804aa3e3
Burlen Loring <bloring@lbl.gov>
2022-12-15 16:03:28 -0800

    rpc_svd_sjlt api improvements
    * support passing pointers
    * pass input matrix const

--- include/RandLAPACK/comps/preconditioners.hh
+++ include/RandLAPACK/comps/preconditioners.hh
@@ -11,8 +11,22 @@ int64_t rpc_svd_sjlt(
     int64_t n, // number of columns in A_rm
     int64_t d, // number of rows in sketch of A_rm
     int64_t k, // number of nonzeros in each column of the sketching operator
-    std::vector<T>& A_rm, // row major
-    std::vector<T>& M_wk, // length at least d*n; 
+    const T *A_rm, // row major
+    T *M_wk, // length at least d*n;
+    T mu,
+    int64_t threads, // number of OpenMP threads to use in sketching.
+    uint64_t seed_key,
+    uint32_t seed_ctr
+);
+
+template <typename T>
+int64_t rpc_svd_sjlt(
+    int64_t m, // number of rows in A_rm
+    int64_t n, // number of columns in A_rm
+    int64_t d, // number of rows in sketch of A_rm
+    int64_t k, // number of nonzeros in each column of the sketching operator
+    const std::vector<T>& A_rm, // row major
+    std::vector<T>& M_wk, // length at least d*n;
     T mu,
     int64_t threads, // number of OpenMP threads to use in sketching.
     uint64_t seed_key,
