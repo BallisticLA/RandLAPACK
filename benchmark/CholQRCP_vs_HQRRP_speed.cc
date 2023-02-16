0c3ea7ea18210ce15a48bac29985baf2b43c0452
TeachRaccooon <m.y.melnich@gmail.com>
2023-02-15 22:22:09 -0500

    Update

--- benchmark/CholQRCP_vs_HQRRP_speed.cc
+++ benchmark/CholQRCP_vs_HQRRP_speed.cc
@@ -361,10 +361,10 @@ test_speed(int r_pow,
 int main(){
     // Run with env OMP_NUM_THREADS=36 numactl --interleave all ./filename 
 
-    for(int num_omp_threads = 36; num_omp_threads <= 36; ++num_omp_threads)
+    for(int num_omp_threads = 8; num_omp_threads <= 8; ++num_omp_threads)
     {
-        test_speed<double>(17, 17, 512, 8192, 5, 64, num_omp_threads, 1, 36, std::pow(std::numeric_limits<double>::epsilon(), 0.75), 1.0, 1.0, std::make_tuple(6, 0, false));
-        test_speed<double>(17, 17, 512, 8192, 5, 128, num_omp_threads, 1, 36, std::pow(std::numeric_limits<double>::epsilon(), 0.75), 1.0, 1.0, std::make_tuple(6, 0, false));
+        test_speed<double>(17, 17, 512, 8192, 5, 32, num_omp_threads, 1, 8, std::pow(std::numeric_limits<double>::epsilon(), 0.75), 1.0, 1.0, std::make_tuple(6, 0, false));
+        //test_speed<double>(17, 17, 512, 8192, 5, 128, num_omp_threads, 1, 36, std::pow(std::numeric_limits<double>::epsilon(), 0.75), 1.0, 1.0, std::make_tuple(6, 0, false));
     }
     return 0;
 }
