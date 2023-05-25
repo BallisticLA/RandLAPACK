53b42d3c5444da614f85c580670143f98a12c123
TeachRaccooon <m.y.melnich@gmail.com>
2023-05-25 13:26:15 -0400

    Reworked SVD testing

--- test/drivers/test_rsvd.cc
+++ test/drivers/test_rsvd.cc
@@ -17,55 +17,77 @@ class TestRSVD : public ::testing::Test
 
     virtual void TearDown() {};
 
+    template <typename T>
+    struct RSVDTestData {
+
+        std::vector<T> A;
+        // For results comparison
+        std::vector<T> A_hat;
+        std::vector<T> A_hat_buf;
+        std::vector<T> A_k;
+        std::vector<T> A_cpy;
+
+        // For RSVD
+        std::vector<T> s1;
+        std::vector<T> S1;
+        std::vector<T> U1;
+        std::vector<T> VT1;
+
+        // For low-rank SVD
+        std::vector<T> s;
+        std::vector<T> S;
+        std::vector<T> U;
+        std::vector<T> VT;
+
+        RSVDTestData(int64_t m, int64_t n, int64_t k) :
+        A(m * n, 0.0),
+        // For results comparison
+        A_hat(m * n, 0.0),
+        A_hat_buf (m * k, 0.0),
+        A_k(m * n, 0.0),
+        A_cpy (m * n, 0.0),
+
+        // For RSVD
+        s1(n, 0.0),
+        S1(n * n, 0.0),
+        U1(m * n, 0.0),
+        VT1(n * n, 0.0),
+
+        // For low-rank SVD
+        s(n, 0.0),
+        S(n * n, 0.0),
+        U(m * n, 0.0),
+        VT(n * n, 0.0)
+        {}
+    };
+
+    template <typename T, typename RNG>
+    static void computational_helper(int64_t m, int64_t n, RSVDTestData<T>& all_data) {
+        
+        // Create a copy of the original matrix
+        blas::copy(m * n, all_data.A.data(), 1, all_data.A_cpy.data(), 1);
+
+        // Get low-rank SVD
+        lapack::gesdd(Job::SomeVec, m, n, all_data.A_cpy.data(), m, all_data.s.data(), all_data.U.data(), m, all_data.VT.data(), n);
+    }
+
     /// General test for RSVD:
     /// Computes the decomposition factors, then checks A-U\Sigma\transpose{V}.
     template <typename T, typename RNG>
-    static void test_RSVD1_general(int64_t m, int64_t n, int64_t k, int64_t p, int64_t block_sz, T tol, std::tuple<int, T, bool> mat_type, RandBLAS::base::RNGState<RNG> state) {
-        
-        printf("|==================================TEST QB2 GENERAL BEGIN==================================|\n");
+    static void test_RSVD1_general(int64_t m, int64_t n, int64_t k, int64_t p, int64_t block_sz, T tol, RandBLAS::base::RNGState<RNG> state, RSVDTestData<T>& all_data) {
 
-        // For running QB
-        std::vector<T> A(m * n, 0.0);
-        RandLAPACK::util::gen_mat_type(m, n, A, k, state, mat_type);
+        T* A_hat_dat = all_data.A_hat.data();
+        T* A_hat_buf_dat = all_data.A_hat_buf.data();
+        T* A_k_dat = all_data.A_k.data();
 
-        int64_t size = m * n;
+        T* U1_dat = all_data.U1.data();
+        T* S1_dat = all_data.S1.data();
+        T* VT1_dat = all_data.VT1.data();
 
-        // For results comparison
-        std::vector<T> A_hat(size, 0.0);
-        std::vector<T> A_hat_buf (m * k, 0.0);
-        std::vector<T> A_k(size, 0.0);
-        std::vector<T> A_cpy (m * n, 0.0);
-
-        // For RSVD
-        std::vector<T> s1(n, 0.0);
-        std::vector<T> S1(n * n, 0.0);
-        std::vector<T> U1(m * n, 0.0);
-        std::vector<T> VT1(n * n, 0.0);
-
-        // For low-rank SVD
-        std::vector<T> s(n, 0.0);
-        std::vector<T> S(n * n, 0.0);
-        std::vector<T> U(m * n, 0.0);
-        std::vector<T> VT(n * n, 0.0);
-
-        T* A_dat = A.data();
-
-        T* A_hat_dat = A_hat.data();
-        T* A_hat_buf_dat = A_hat_buf.data();
-        T* A_k_dat = A_k.data();
-        T* A_cpy_dat = A_cpy.data();
-
-        T* U1_dat = U1.data();
-        T* S1_dat = S1.data();
-        T* VT1_dat = VT1.data();
-
-        T* U_dat = U.data();
-        T* s_dat = s.data();
-        T* S_dat = S.data();
-        T* VT_dat = VT.data();
-
-        // Create a copy of the original matrix
-        blas::copy(size, A_dat, 1, A_cpy_dat, 1);
+        T* U_dat = all_data.U.data();
+        T* s_dat = all_data.s.data();
+        T* S_dat = all_data.S.data();
+        T* VT_dat = all_data.VT.data();
 
         //Subroutine parameters 
         bool verbosity = false;
@@ -96,12 +118,12 @@ class TestRSVD : public ::testing::Test
         RandLAPACK::RSVD<T> RSVD(QB, verbosity, block_sz);
 
         // Regular QB2 call
-        RSVD.call(m, n, A, k, tol, U1, s1, VT1);
+        RSVD.call(m, n, all_data.A, k, tol, all_data.U1, all_data.s1, all_data.VT1);
         
         // Construnct A_hat = U1 * S1 * VT1
 
         // Turn vector into diagonal matrix
-        RandLAPACK::util::diag(k, k, s1, k, S1);
+        RandLAPACK::util::diag(k, k, all_data.s1, k, all_data.S1);
         // U1 * S1 = A_hat_buf
         blas::gemm(Layout::ColMajor, Op::NoTrans, Op::NoTrans, m, k, k, 1.0, U1_dat, m, S1_dat, k, 1.0, A_hat_buf_dat, m);
         // A_hat_buf * VT1 =  A_hat
@@ -110,15 +132,11 @@ class TestRSVD : public ::testing::Test
         //T norm_test_4 = lapack::lange(Norm::Fro, m, n, A_cpy_dat, m);
         //printf("FRO NORM OF A_k - QB:  %e\n", norm_test_4);
 
-
-        // Reconstruct  a standard low-rank SVD
-        lapack::gesdd(Job::SomeVec, m, n, A_cpy_dat, m, s_dat, U_dat, m, VT_dat, n);
         // buffer zero vector
         std::vector<T> z_buf(n, 0.0);
-        T* z_buf_dat = z_buf.data();
         // zero out the trailing singular values
-        blas::copy(n - k, z_buf_dat, 1, s_dat + k, 1);
-        RandLAPACK::util::diag(n, n, s, n, S);
+        blas::copy(n - k, z_buf.data(), 1, s_dat + k, 1);
+        RandLAPACK::util::diag(n, n, all_data.s, n, all_data.S);
 
         // TEST 4: Below is A_k - A_hat = A_k - QB
         blas::gemm(Layout::ColMajor, Op::NoTrans, Op::NoTrans, m, n, n, 1.0, U_dat, m, S_dat, n, 1.0, A_k_dat, m);
@@ -134,7 +152,16 @@ class TestRSVD : public ::testing::Test
 
 TEST_F(TestRSVD, SimpleTest)
 { 
-    // Generate a random state
+    int64_t m = 100;
+    int64_t n = 100;
+    int64_t k = 5;
+    int64_t p = 10;
+    int64_t block_sz = 2;
+    double tol = std::pow(std::numeric_limits<double>::epsilon(), 0.5625);
     auto state = RandBLAS::base::RNGState();
-    test_RSVD1_general<double, r123::Philox4x32>(100, 100, 50, 5, 10, std::pow(std::numeric_limits<double>::epsilon(), 0.5625), std::make_tuple(0, 2, false), state);
+    RSVDTestData<double> all_data(m, n, k);
+
+    RandLAPACK::util::gen_mat_type<double, r123::Philox4x32>(m, n, all_data.A, k, state, std::make_tuple(0, 2, false));
+    computational_helper<double, r123::Philox4x32>(m, n, all_data);
+    test_RSVD1_general<double, r123::Philox4x32>(m, n, k, p, block_sz, tol, state, all_data);
 }
