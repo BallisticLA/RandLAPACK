b9bc01f45b803e6f318d54ea762ac25ec46a8e90
TeachRaccooon <m.y.melnich@gmail.com>
2023-05-25 13:35:16 -0400

    Reworked Orth test

--- test/comps/test_orth.cc
+++ test/comps/test_orth.cc
@@ -24,41 +24,52 @@ class TestOrth : public ::testing::Test
 
     virtual void TearDown() {};
 
+    template <typename T>
+    struct OrthTestData {
+        std::vector<T> A;
+        std::vector<T> Y;
+        std::vector<T> Omega;
+        std::vector<T> I_ref;
+
+        OrthTestData(int64_t m, int64_t n, int64_t k) :
+        A(m * n, 0.0),
+        Y(m * k, 0.0),
+        Omega(n * k, 0.0),
+        I_ref(k * k, 0.0)
+        {}
+    };
+
+    template <typename T, typename RNG>
+    static void computational_helper(int64_t m, int64_t n, int64_t k, RandBLAS::base::RNGState<RNG> state, OrthTestData<T>& all_data) {
+        // Fill the gaussian random matrix
+        RandBLAS::dense::DenseDist D{.n_rows = n, .n_cols = k};
+        state = RandBLAS::dense::fill_buff(all_data.Omega.data(), D, state);
+        
+        // Generate a reference identity
+        RandLAPACK::util::eye(k, k, all_data.I_ref);
+
+        // Y = A * Omega
+        blas::gemm(Layout::ColMajor, Op::NoTrans, Op::NoTrans, m, k, n, 1.0, all_data.A.data(), m, all_data.Omega.data(), n, 0.0, all_data.Y.data(), m);
+    }
+
     /// Tests orthogonality of a matrix Q, obtained by orthogonalizing a Gaussian sketch.
     /// Checks I - \transpose{Q}Q.
     template <typename T, typename RNG>
-    static void test_orth_sketch(int64_t m, int64_t n, int64_t k, std::tuple<int, T, bool> mat_type, RandBLAS::base::RNGState<RNG> state) {
+    static void test_orth_sketch(int64_t m, int64_t n, int64_t k, RandBLAS::base::RNGState<RNG> state, OrthTestData<T>& all_data) {
 
-        int64_t size = m * n;
-        std::vector<T> A(size, 0.0);
-        std::vector<T> Y(m * k, 0.0);
-        std::vector<T> Omega(n * k, 0.0);
-        std::vector<T> I_ref(k * k, 0.0);
+        T* Y_dat = all_data.Y.data();
+        T* I_ref_dat = all_data.I_ref.data();
 
-        T* A_dat = A.data();
-        T* Y_dat = Y.data();
-        T* Omega_dat = Omega.data();
-        T* I_ref_dat = I_ref.data();
-
-        RandLAPACK::util::gen_mat_type(m, n, A, k, state, mat_type);
-
-        // Fill the gaussian random matrix
-        RandBLAS::dense::DenseDist D{.n_rows = n, .n_cols = k};
-        state = RandBLAS::dense::fill_buff(Omega_dat, D, state);
-        // Generate a reference identity
-        RandLAPACK::util::eye(k, k, I_ref);
-        // Y = A * Omega
-        blas::gemm(Layout::ColMajor, Op::NoTrans, Op::NoTrans, m, k, n, 1.0, A_dat, m, Omega_dat, n, 0.0, Y_dat, m);
         // Orthogonalization Constructor
         RandLAPACK::CholQRQ<T> CholQRQ(false, false);
 
         // Orthonormalize sketch Y
-        if(CholQRQ.call(m, k, Y) != 0) {
+        if(CholQRQ.call(m, k, all_data.Y) != 0) {
             EXPECT_TRUE(false) << "\nPOTRF FAILED DURE TO ILL-CONDITIONED DATA\n";
             return;
         }
         // Call the scheme twice for better orthogonality
-        CholQRQ.call(m, k, Y);
+        CholQRQ.call(m, k, all_data.Y);
         // Q' * Q  - I = 0
         blas::syrk(Layout::ColMajor, Uplo::Upper, Op::Trans, k, m, 1.0, Y_dat, m, -1.0, I_ref_dat, k);
         T norm_fro = lapack::lansy(lapack::Norm::Fro, Uplo::Upper, k, I_ref_dat, k);
@@ -69,6 +80,15 @@ class TestOrth : public ::testing::Test
     }
 };
 
-TEST_F(TestOrth, SimpleTest)
+TEST_F(TestOrth, Test_CholQRQ)
 {
+    int64_t m = 1000;
+    int64_t n = 200;
+    int64_t k = 200;
+    auto state = RandBLAS::base::RNGState();
+    OrthTestData<double> all_data(m, n, k);
+
+    RandLAPACK::util::gen_mat_type<double, r123::Philox4x32>(m, n, all_data.A, k, state, std::make_tuple(0, 2, false));
+    computational_helper<double, r123::Philox4x32>(m, n, k, state, all_data);
+    test_orth_sketch<double, r123::Philox4x32>(m, n, k, state, all_data);
 }
