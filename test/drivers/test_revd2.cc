c34ec220a88fb8e62b34a42fd73b62a0cc584f23
TeachRaccooon <m.y.melnich@gmail.com>
2023-06-02 14:48:25 -0400

    Adding a test with Strictly Upper and Lower triangular input matrices for REVD2

--- test/drivers/test_revd2.cc
+++ test/drivers/test_revd2.cc
@@ -40,6 +40,38 @@ class TestREVD2 : public ::testing::Test
         }
     };
 
+        template <typename T>
+    struct REVD2UploTestData {
+        int64_t dim;
+        int64_t rank;
+        std::vector<T> work;
+        std::vector<T> A_u;
+        std::vector<T> V_u;
+        std::vector<T> eigvals_u;
+        std::vector<T> A_l;
+        std::vector<T> V_l;
+        std::vector<T> eigvals_l;
+        std::vector<T> E_u;
+        std::vector<T> E_l;
+
+        REVD2UploTestData(
+            int64_t m, int64_t k
+        ) : 
+            work(m * m, 0.0),
+            A_u(m * m, 0.0),
+            V_u(m * k, 0.0), 
+            eigvals_u(k, 0.0), 
+            A_l(m * m, 0.0),
+            V_l(m * k, 0.0), 
+            eigvals_l(k, 0.0),
+            E_u(k * k, 0.0), 
+            E_l(k * k, 0.0)
+        {
+            dim = m;
+            rank = k;
+        }
+    };
+
     template <typename T, typename RNG>
     struct algorithm_objects {
         RandLAPACK::SYPS<T, RNG> SYPS;
@@ -80,6 +112,31 @@ class TestREVD2 : public ::testing::Test
         norm_A = lapack::lange(Norm::Fro, m, m, all_data.A_cpy.data(), m);
     }
 
+    template <typename T, typename RNG>
+    static void uplo_computational_helper(REVD2UploTestData<T>& all_data) {
+        auto m = all_data.dim;
+        // Filling the upper-triangular matrix
+        blas::syrk(Layout::ColMajor, Uplo::Upper, Op::Trans, m, m, 1.0, all_data.work.data(), m, 0.0, all_data.A_u.data(), m);
+        // Filling the lower-triangular matrix
+        blas::syrk(Layout::ColMajor, Uplo::Lower, Op::Trans, m, m, 1.0, all_data.work.data(), m, 0.0, all_data.A_l.data(), m);
+
+        // Fill the unused space with NANs
+        T* A_u_dat = all_data.A_u.data();
+        T* A_l_dat = all_data.A_l.data();
+
+        /*
+        std::fill(&A_u_dat[1], &A_u_dat[m], 0.0);
+        for(int i = 1; i < m; ++i) {
+            std::fill(&A_l_dat[m * i], &A_l_dat[i + m * i], 0.0);
+            std::fill(&A_u_dat[i + m * i], &A_u_dat[m * (i + 1)], 0.0);
+        }
+        */
+
+        char name [] = "A";
+        RandBLAS::util::print_colmaj(m, m, all_data.A_u.data(), name);
+        RandBLAS::util::print_colmaj(m, m, all_data.A_l.data(), name);
+    }
+
     /// General test for REVD:
     /// Computes the decomposition factors, then checks A-U\Sigma\transpose{V}.
     template <typename T, typename RNG>
@@ -118,6 +175,50 @@ class TestREVD2 : public ::testing::Test
         printf("||A - VEV'||_F / ||A||_F:  %e\n", norm_0 / norm_A);
         ASSERT_NEAR(norm_0 / norm_A, err_expectation, 10 * err_expectation);
         ASSERT_NEAR(k, rank_expectation, std::numeric_limits<T>::epsilon());
+    }
+
+        /// General test for REVD:
+    /// Computes the decomposition factors, then checks A-U\Sigma\transpose{V}.
+    template <typename T, typename RNG>
+    static void test_REVD2_uplo(
+        int64_t k_start, 
+        T tol,
+        T err_expectation, 
+        REVD2UploTestData<T>& all_data,
+        algorithm_objects<T, RNG>& all_algs,
+        RandBLAS::base::RNGState<RNG> state
+    ) {
+        
+        auto m = all_data.dim;
+
+        int64_t k = k_start;
+        all_algs.REVD2.call(blas::Uplo::Upper, m, all_data.A_u, k, tol, all_data.V_u, all_data.eigvals_u, state);
+        all_algs.REVD2.call(blas::Uplo::Lower, m, all_data.A_l, k, tol, all_data.V_l, all_data.eigvals_l, state);
+
+        T* E_u_dat = RandLAPACK::util::upsize(k * k, all_data.E_u);
+        T* E_l_dat = RandLAPACK::util::upsize(k * k, all_data.E_l);
+        T* V_u_dat = all_data.V_u.data();
+        T* V_l_dat = all_data.V_l.data();
+        T* work_u_dat = all_data.A_u.data();
+        T* work_l_dat = all_data.A_l.data();
+        T* A_approx_dat = all_data.work.data();
+
+        RandLAPACK::util::diag(k, k, all_data.eigvals_u, k, all_data.E_u);
+        RandLAPACK::util::diag(k, k, all_data.eigvals_l, k, all_data.E_l);
+
+        // Reconstruct factorizations, compare the result
+        // V_u * E_u = work_u
+        blas::gemm(Layout::ColMajor, Op::NoTrans, Op::NoTrans, m, k, k, 1.0, V_u_dat, m, E_u_dat, k, 0.0, work_u_dat, m);
+        // work_u * V_u' = A_approx
+        blas::gemm(Layout::ColMajor, Op::NoTrans, Op::Trans, m, m, k, 1.0, work_u_dat, m, V_u_dat, m, 0.0, A_approx_dat, m);
+        // V_l * E_l = work_l
+        blas::gemm(Layout::ColMajor, Op::NoTrans, Op::NoTrans, m, k, k, 1.0, V_l_dat, m, E_l_dat, k, 0.0, work_l_dat, m);
+        // work_l * V_l' - A_approx
+        blas::gemm(Layout::ColMajor, Op::NoTrans, Op::Trans, m, m, k, 1.0, work_l_dat, m, V_l_dat, m, -1.0, A_approx_dat, m);
+
+        T norm = lapack::lange(Norm::Fro, m, m, A_approx_dat, m);
+        printf("||V_u*E_u*V_u' - V_l*E_l*V_l'||_F  %e\n", norm);
+        ASSERT_NEAR(norm, 0.0, 10 * err_expectation);
     }
 };
 
@@ -293,3 +394,33 @@ TEST_F(TestREVD2, Exactness) {
     );
 }
 
+TEST_F(TestREVD2, Uplo) { 
+    using RNG = r123::Philox4x32;
+
+    int64_t m = 10;
+    int64_t k = 5;
+    int64_t k_start = 1;
+    double tol = std::pow(10, -14);
+    double err_expectation = std::pow(10, -13);
+    int64_t num_syps_passes = 3;
+    int64_t passes_per_syps_stabilization = 1;
+    int64_t num_steps_power_iter_error_est = 10;
+    auto state = RandBLAS::base::RNGState(0);
+    //Subroutine parameters 
+    bool verbose = false;
+    bool cond_check = false;
+
+    REVD2UploTestData<double> all_data(m, k);
+    algorithm_objects<double, RNG> all_algs(
+        verbose, cond_check,
+        num_syps_passes, 
+        passes_per_syps_stabilization, 
+        num_steps_power_iter_error_est
+    );
+
+    RandLAPACK::util::gen_mat_type<double, RNG>(m, m, all_data.work, k, state, std::make_tuple(0, std::pow(10, 2), false));
+
+    uplo_computational_helper<double, RNG>(all_data);
+    
+    test_REVD2_uplo<double, RNG>(k_start, tol, err_expectation, all_data, all_algs, state);
+}
