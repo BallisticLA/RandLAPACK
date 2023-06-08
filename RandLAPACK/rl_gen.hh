57076f6f3a22a64daa4f9cb5d35b04dee55dfc17
TeachRaccooon <m.y.melnich@gmail.com>
2023-06-08 14:10:53 -0700

    Pre-Migrating matrix generation into a separate file.

---
+++ RandLAPACK/rl_gen.hh
@@ -0,0 +1,17 @@
+#ifndef randlapack_gen_h
+#define randlapack_gen_h
+
+#include "rl_blaspp.hh"
+#include "rl_lapackpp.hh"
+
+#include <RandBLAS.hh>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <cstdint>
+
+namespace RandLAPACK::gen {
+
+}
+#endif
