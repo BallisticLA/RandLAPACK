520cbe8d4dba122768156d2b5291e2efac08535f
TeachRaccooon <m.y.melnich@gmail.com>
2023-08-08 19:59:17 -0700

    For a large size and sampling factor != 1, there is a bug. Check if it was present before the output format switch.

--- test/drivers/test_cqrrp.cc
+++ test/drivers/test_cqrrp.cc
@@ -141,11 +141,11 @@ class TestCQRRP : public ::testing::Test
 
 // Note: If Subprocess killed exception -> reload vscode
 TEST_F(TestCQRRP, CQRRP_blocked_full_rank_no_hqrrp) {
-    int64_t m = 5000;
-    int64_t n = 2000;
-    int64_t k = 1600;
-    int64_t d_factor = 1.0;
-    int64_t b_sz = 500;
+    int64_t m = 12;
+    int64_t n = 12;
+    int64_t k = 8;
+    int64_t d_factor = 2.0;
+    int64_t b_sz = 4;
     double norm_A = 0;
     double tol = std::pow(std::numeric_limits<double>::epsilon(), 0.85);
     auto state = RandBLAS::RNGState();
