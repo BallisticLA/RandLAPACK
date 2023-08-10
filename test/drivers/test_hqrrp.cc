75e24cc27469ba29a74710f07bd3fd41d54d5db6
TeachRaccooon <m.y.melnich@gmail.com>
2023-08-10 10:34:36 -0700

    Added testing for HQRRP

---
+++ test/drivers/test_hqrrp.cc
@@ -0,0 +1,163 @@
+#include "RandLAPACK.hh"
+#include "rl_blaspp.hh"
+#include "rl_lapackpp.hh"
+#include "rl_gen.hh"
+
+#include <RandBLAS.hh>
+#include <fstream>
+#include <gtest/gtest.h>
+
+
+class TestHQRRP : public ::testing::Test
+{
+    protected:
+
+    virtual void SetUp() {};
+
+    virtual void TearDown() {};
+
+    template <typename T>
+    struct HQRRPtestData {
+        int64_t row;
+        int64_t col;
+        int64_t rank; // has to be modifiable
+        std::vector<T> A;
+        std::vector<T> Q;
+        std::vector<T> R;
+        std::vector<T> tau;
+        std::vector<int64_t> J;
+        std::vector<T> A_cpy1;
+        std::vector<T> A_cpy2;
+        std::vector<T> I_ref;
+
+        HQRRPtestData(int64_t m, int64_t n, int64_t k) :
+        A(m * n, 0.0),
+        Q(m * n, 0.0),
+        tau(n, 0.0),
+        J(n, 0),
+        A_cpy1(m * n, 0.0),
+        A_cpy2(m * n, 0.0),
+        I_ref(k * k, 0.0) 
+        {
+            row = m;
+            col = n;
+            rank = k;
+        }
+    };
+
+    template <typename T, typename RNG>
+    static void norm_and_copy_computational_helper(T &norm_A, HQRRPtestData<T> &all_data) {
+        auto m = all_data.row;
+        auto n = all_data.col;
+
+        lapack::lacpy(MatrixType::General, m, n, all_data.A.data(), m, all_data.A_cpy1.data(), m);
+        lapack::lacpy(MatrixType::General, m, n, all_data.A.data(), m, all_data.A_cpy2.data(), m);
+        norm_A = lapack::lange(Norm::Fro, m, n, all_data.A.data(), m);
+    }
+
+
+    /// This routine also appears in benchmarks, but idk if it should be put into utils
+    template <typename T>
+    static void
+    error_check(T &norm_A, HQRRPtestData<T> &all_data) {
+
+        auto m = all_data.row;
+        auto n = all_data.col;
+        auto k = all_data.rank;
+
+        RandLAPACK::util::upsize(k * k, all_data.I_ref);
+        RandLAPACK::util::eye(k, k, all_data.I_ref);
+
+        T* A_dat         = all_data.A_cpy1.data();
+        T const* A_cpy_dat = all_data.A_cpy2.data();
+        T const* Q_dat   = all_data.Q.data();
+        T const* R_dat   = all_data.R.data();
+        T* I_ref_dat     = all_data.I_ref.data();
+
+        // Check orthogonality of Q
+        // Q' * Q  - I = 0
+        blas::syrk(Layout::ColMajor, Uplo::Upper, Op::Trans, k, m, 1.0, Q_dat, m, -1.0, I_ref_dat, k);
+        T norm_0 = lapack::lansy(lapack::Norm::Fro, Uplo::Upper, k, I_ref_dat, k);
+
+        // A - QR
+        blas::gemm(Layout::ColMajor, Op::NoTrans, Op::NoTrans, m, n, k, 1.0, Q_dat, m, R_dat, k, -1.0, A_dat, m);
+        
+        // Implementing max col norm metric
+        T max_col_norm = 0.0;
+        T col_norm = 0.0;
+        int max_idx = 0;
+        for(int i = 0; i < n; ++i) {
+            col_norm = blas::nrm2(m, &A_dat[m * i], 1);
+            if(max_col_norm < col_norm) {
+                max_col_norm = col_norm;
+                max_idx = i;
+            }
+        }
+        T col_norm_A = blas::nrm2(n, &A_cpy_dat[m * max_idx], 1);
+        T norm_AQR = lapack::lange(Norm::Fro, m, n, A_dat, m);
+        
+        printf("REL NORM OF AP - QR:    %14e\n", norm_AQR / norm_A);
+        printf("MAX COL NORM METRIC:    %14e\n", max_col_norm / col_norm_A);
+        printf("FRO NORM OF (Q'Q - I):  %14e\n\n", norm_0 / std::sqrt((T) n));
+
+        T atol = std::pow(std::numeric_limits<T>::epsilon(), 0.75);
+        ASSERT_NEAR(norm_AQR / norm_A,         0.0, atol);
+        ASSERT_NEAR(max_col_norm / col_norm_A, 0.0, atol);
+        ASSERT_NEAR(norm_0, 0.0, atol);
+    }
+
+    /// General test for HQRRP:
+    /// Computes QR factorzation, and computes A[:, J] - QR.
+    template <typename T, typename RNG>
+    static void test_HQRRP_general(
+        T d_factor, 
+        int64_t b_sz, 
+        int panel_pivoting, 
+        T norm_A,
+        HQRRPtestData<T> &all_data,
+        RandBLAS::RNGState<RNG> &state) {
+
+        auto m = all_data.row;
+        auto n = all_data.col;
+
+        RandLAPACK::hqrrp(m, n, all_data.A.data(), m, all_data.J.data(), all_data.tau.data(), b_sz, (int64_t) (d_factor * b_sz), panel_pivoting, state);
+
+        RandLAPACK::util::upsize(all_data.rank * n, all_data.R);
+        lapack::lacpy(MatrixType::Upper, all_data.rank, n, all_data.A.data(), m, all_data.R.data(), all_data.rank);
+
+        lapack::ungqr(m, n, n, all_data.A.data(), m, all_data.tau.data());
+        lapack::lacpy(MatrixType::General, m, all_data.rank, all_data.A.data(), m, all_data.Q.data(), m);
+
+        // I don't think hqrrp actually returns anything.
+        //printf("RANK AS RETURNED BY HQRRP %4ld\n", all_data.rank);
+
+        RandLAPACK::util::col_swap(m, n, n, all_data.A_cpy1.data(), m, all_data.J);
+        RandLAPACK::util::col_swap(m, n, n, all_data.A_cpy2.data(), m, all_data.J);
+
+        error_check(norm_A, all_data); 
+    }
+};
+
+// Note: If Subprocess killed exception -> reload vscode
+TEST_F(TestHQRRP, HQRRP_full_rank_no_hqrrp) {
+    int64_t m = 5000;
+    int64_t n = 2000;
+    int64_t k = 1600;
+    double d_factor = 1.0;
+    int64_t b_sz = 500;
+    int panel_pivoting = 1;
+    double norm_A = 0;
+    double tol = std::pow(std::numeric_limits<double>::epsilon(), 0.85);
+    auto state = RandBLAS::RNGState();
+
+    HQRRPtestData<double> all_data(m, n, k);
+
+    RandLAPACK::gen::mat_gen_info<double> m_info(m, n, RandLAPACK::gen::polynomial);
+    m_info.cond_num = 2;
+    m_info.rank = k;
+    m_info.exponent = 2.0;
+    RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.A, state);
+
+    norm_and_copy_computational_helper<double, r123::Philox4x32>(norm_A, all_data);
+    test_HQRRP_general<double, r123::Philox4x32>(d_factor, b_sz, panel_pivoting, norm_A, all_data, state);
+}
