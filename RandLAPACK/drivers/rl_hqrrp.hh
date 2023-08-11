cb7cd13c27ec6813de0861c65aab8985fa980a71
TeachRaccooon <m.y.melnich@gmail.com>
2023-08-11 13:15:16 -0700

    Bug fix

--- RandLAPACK/drivers/rl_hqrrp.hh
+++ RandLAPACK/drivers/rl_hqrrp.hh
@@ -569,7 +569,7 @@ static int64_t CHOLQR_mod_WY(
     free( buff_R );
 
     high_resolution_clock::time_point timing_t_stop;
-    printf("Time it took to do CHOLQR in HQRRP %ld\n", duration_cast<microseconds>(timing_t_stop - timing_t_start).count())
+    printf("Time it took to do CHOLQR in HQRRP %ld\n", duration_cast<microseconds>(timing_t_stop - timing_t_start).count());
     return 0;
 }
 
