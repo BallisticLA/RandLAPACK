5f89ea79323cbd27013bc6a4243195470c21bad9
TeachRaccooon <m.y.melnich@gmail.com>
2023-08-17 10:17:54 -0700

    Update

--- benchmark/CQRRP_speed_comparisons.cc
+++ benchmark/CQRRP_speed_comparisons.cc
@@ -193,11 +193,11 @@ static std::vector<long> call_all_algs(
 
 int main() {
     // Declare parameters
-    int64_t m          = 1.125 * 256;//std::pow(2, 14);
+    int64_t m          = 1.125 * 1024;//256;
     int64_t n          = std::pow(2, 14);
     int64_t d_factor   = 1.125;
-    int64_t b_sz_start = 8;
-    int64_t b_sz_end   = 256;//2048;
+    int64_t b_sz_start = 128;//32;
+    int64_t b_sz_end   = 1024;//256;
     double tol         = std::pow(std::numeric_limits<double>::epsilon(), 0.85);
     auto state         = RandBLAS::RNGState();
     auto state_constant = state;
