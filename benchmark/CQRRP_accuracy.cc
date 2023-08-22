c2d2a40c0c3b270d5dab76442633eaa0741b2e0e
TeachRaccooon <m.y.melnich@gmail.com>
2023-08-22 08:55:48 -0700

    Update

--- benchmark/CQRRP_accuracy.cc
+++ benchmark/CQRRP_accuracy.cc
@@ -167,9 +167,9 @@ static void sv_ratio(
 int main() {
     // Declare parameters
     int64_t m          = std::pow(2, 14);
-    int64_t n          = 1024;//std::pow(2, 14);
+    int64_t n          = 768;//std::pow(2, 14);
     int64_t d_factor   = 1.125;
-    int64_t b_sz       = 768;
+    int64_t b_sz       = 256;
     double tol         = std::pow(std::numeric_limits<double>::epsilon(), 0.85);
     auto state         = RandBLAS::RNGState();
     auto state_constant1 = state;
