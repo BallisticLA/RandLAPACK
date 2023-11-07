046906da2052bd3193b9a17f56a9862b7a93472b
TeachRaccooon <m.y.melnich@gmail.com>
2023-11-07 11:10:30 -0800

    Added CQRRPT benchmarks. Next step - reworking CQRRPT

---
+++ benchmark/bench_CQRRPT/CQRRPT_inner_speed.cc
@@ -0,0 +1,121 @@
+#include "RandLAPACK.hh"
+#include "rl_blaspp.hh"
+#include "rl_lapackpp.hh"
+#include "rl_gen.hh"
+
+#include <RandBLAS.hh>
+#include <fstream>
+
+template <typename T>
+struct QR_benchmark_data {
+    int64_t row;
+    int64_t col;
+    T       tolerance;
+    T       sampling_factor;
+    std::vector<T> A;
+    std::vector<T> R;
+    std::vector<T> tau;
+    std::vector<int64_t> J;
+
+    QR_benchmark_data(int64_t m, int64_t n, T tol, T d_factor) :
+    A(m * n, 0.0),
+    R(n, n),
+    tau(n, 0.0),
+    J(n, 0)
+    {
+        row             = m;
+        col             = n;
+        tolerance       = tol;
+        sampling_factor = d_factor;
+    }
+};
+
+// Re-generate and clear data
+template <typename T, typename RNG>
+static void data_regen(RandLAPACK::gen::mat_gen_info<T> m_info, 
+                                        QR_benchmark_data<T> &all_data, 
+                                        RandBLAS::RNGState<RNG> &state) {
+
+    RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.A, state);
+    std::fill(all_data.tau.begin(), all_data.tau.end(), 0.0);
+    std::fill(all_data.J.begin(), all_data.J.end(), 0);
+}
+
+template <typename T, typename RNG>
+static std::vector<long> call_all_algs(
+    RandLAPACK::gen::mat_gen_info<T> m_info,
+    int64_t numruns,
+    int64_t n,
+    QR_benchmark_data<T> &all_data,
+    RandBLAS::RNGState<RNG> &state) {
+
+    auto m        = all_data.row;
+    auto tol      = all_data.tolerance;
+    auto d_factor = all_data.sampling_factor;
+
+    // Additional params setup.
+    RandLAPACK::CQRRPT<double, r123::Philox4x32> CQRRPT(true, true, tol);
+    CQRRPT.nnz = 4;
+    CQRRPT.num_threads = 48;
+    
+    // Making sure the states are unchanged
+    auto state_alg = state;
+    auto state_gen = state;
+
+    // Timing vars
+    long dur_cqrrpt    = 0;
+    long t_cqrrpt_best = 0;
+    std::vector<long> inner_timing_best;
+
+    for (int i = 0; i < numruns; ++i) {
+        printf("Iteration %d start.\n", i);
+
+        auto start_cqrrpt = high_resolution_clock::now();
+        CQRRPT.call(m, n, all_data.A, d_factor * n, all_data.R, all_data.J, state_alg);
+        auto stop_cqrrpt = high_resolution_clock::now();
+        dur_cqrrpt = duration_cast<microseconds>(stop_cqrrpt - start_cqrrpt).count();
+        // Update best timing
+        if (!i || dur_cqrrpt < t_cqrrpt_best) {t_cqrrpt_best = dur_cqrrpt; inner_timing_best = CQRRPT.times;}
+
+        // Making sure the states are unchanged
+        state_alg = state;
+        state_gen = state;
+        // Clear and re-generate data
+        data_regen<T, RNG>(m_info, all_data, state_gen);
+    }
+
+    return inner_timing_best;
+}
+
+int main() {
+    // Declare parameters
+    int64_t m           = std::pow(2, 17);
+    int64_t n_start     = std::pow(2, 5);
+    int64_t n_stop      = std::pow(2, 14);
+    double  d_factor    = 1.25;
+    double tol          = std::pow(std::numeric_limits<double>::epsilon(), 0.85);
+    auto state          = RandBLAS::RNGState();
+    auto state_constant = state;
+    // Timing results
+    std::vector<long> res;
+    // Number of algorithm runs. We only record best times.
+    int64_t numruns = 25;
+
+    // Allocate basic workspace at its max size.
+    QR_benchmark_data<double> all_data(m, n_stop, tol, d_factor);
+    // Generate the input matrix - gaussian suffices for performance tests.
+    RandLAPACK::gen::mat_gen_info<double> m_info(m, n_stop, RandLAPACK::gen::gaussian);
+    RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.A, state);
+
+    // Declare a data file
+    std::fstream file("CQRRPT_inner_speed_"              + std::to_string(m)
+                                      + "_col_start_"    + std::to_string(n_start)
+                                      + "_col_stop_"     + std::to_string(n_stop)
+                                      + "_d_factor_"     + std::to_string(d_factor)
+                                      + ".dat", std::fstream::app);
+
+    for (;n_start <= n_stop; n_start *= 2) {
+        res = call_all_algs<double, r123::Philox4x32>(m_info, numruns, n_start, all_data, state_constant);
+        file << res[0]  << ",  " << res[1]  << ",  " << res[2] << ",  " << res[3] << ",  " << res[4] << ",  " << res[5] << ",  " << res[6] << ",  " << res[7] << ",  " << res[8] << ",  " << ",\n";
+    }
+}
---
+++ benchmark/bench_general/Chol_check.cc
@@ -0,0 +1,57 @@
+#include "RandLAPACK.hh"
+#include "rl_blaspp.hh"
+
+#include <RandBLAS.hh>
+
+using namespace RandLAPACK;
+
+template <typename T, typename RNG>
+static void 
+chol_check(int64_t m, int64_t k, RandBLAS::RNGState<RNG> state) {
+
+    std::vector<T> A(m * m, 0.0);
+    std::vector<T> A_leading_submat_symm(k * k, 0.0);
+    std::vector<T> R_submat(k * k, 0.0);
+
+    RandLAPACK::gen::mat_gen_info<double> m_info(m, m, RandLAPACK::gen::polynomial);
+    m_info.cond_num = std::pow(10, 8);
+    RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, A, state);
+
+    T* A_dat = A.data();
+    T* A_leading_submat_symm_dat = A_leading_submat_symm.data();
+    T* R_submat_dat = R_submat.data();
+
+    blas::syrk(Layout::ColMajor, Uplo::Upper, Op::Trans, k, k, 1.0, A.data(), k, 0.0, A_leading_submat_symm.data(), k);
+
+    A[0] = A_leading_submat_symm[0];
+    for(int i = 1; i < k; ++i) {
+        // Fill the upper-triangular part
+        blas::copy(i + 1, &A_leading_submat_symm_dat[(i * k)], 1, &A_dat[i * m], 1);
+        // Fill the lower-triangular part
+        blas::copy(k - i, &A_leading_submat_symm_dat[(i - 1) + (i * k)], k, &A_dat[i + ((i-1) * m)], 1);
+        // Also fill the full symmetric matrix
+        blas::copy(k - i, &A_leading_submat_symm_dat[(i - 1) + (i * k)], k, &A_leading_submat_symm_dat[i + ((i-1) * k)], 1);
+    }
+
+    // Now, the k by k portion of A is summetric and the rest is random
+
+    if(lapack::potrf(Uplo::Upper, m, A_dat, m)) 
+        printf("Cholesky failed as expected.\n");
+
+    // Copy the k by k portion of the R factor
+    lapack::lacpy(MatrixType::Upper, k, k, A_dat, m, R_submat_dat, k);
+
+    // Doing this through GEMM to perform subtraction immediately
+    blas::gemm(Layout::ColMajor, Op::Trans, Op::NoTrans, k, k, k, 1.0, R_submat_dat, k, R_submat_dat, k, -1.0, A_leading_submat_symm_dat, k);
+
+    T norm = lapack::lange(Norm::Fro, k, k, A_leading_submat_symm_dat, k);
+    printf("||R[:k, :k]'*R[:k, :k] - A[:k, :k]||_F:  %e\n", norm);
+}
+
+int main() {
+    for(int i = 0; i < 10; ++i) {
+        auto state = RandBLAS::RNGState(i);
+        chol_check<double, r123::Philox4x32>(1000, 500, state);
+    }
+    return 0;
+}
