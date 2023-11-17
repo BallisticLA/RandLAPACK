d5b4d2cd48fdaf93962b6666ac352982e7989879
TeachRaccooon <m.y.melnich@gmail.com>
2023-11-17 06:38:18 -0800

    Benchmark update

--- benchmark/bench_RBKI/RBKI_speed_comparisons.cc
+++ benchmark/bench_RBKI/RBKI_speed_comparisons.cc
@@ -7,192 +7,130 @@
 #include <fstream>
 
 template <typename T>
-struct QR_benchmark_data {
+struct RBKI_benchmark_data {
     int64_t row;
     int64_t col;
-    T       tolerance;
-    T sampling_factor;
+    int64_t rank; // has to be modifiable
+    T tolerance;
     std::vector<T> A;
-    std::vector<T> R;
-    std::vector<T> tau;
-    std::vector<int64_t> J;
+    std::vector<T> U;
+    std::vector<T> V;
+    std::vector<T> Sigma;
+    std::vector<T> A_cpy;
+    std::vector<T> Sigma_exact;
 
-    QR_benchmark_data(int64_t m, int64_t n, T tol, T d_factor) :
+    RBKI_benchmark_data(int64_t m, int64_t n, int64_t k, T tol) :
     A(m * n, 0.0),
-    R(n * n, 0.0),
-    tau(n, 0.0),
-    J(n, 0)
+    U(m * n, 0.0),
+    V(n * n, 0.0),
+    Sigma(n, 0.0),
+    A_cpy(m * n, 0.0),
+    Sigma_exact(n, 0.0)
     {
-        row             = m;
-        col             = n;
-        tolerance       = tol;
-        sampling_factor = d_factor;
+        row = m;
+        col = n;
+        rank = k;
+        tolerance = tol;
     }
 };
 
 // Re-generate and clear data
 template <typename T, typename RNG>
 static void data_regen(RandLAPACK::gen::mat_gen_info<T> m_info, 
-                                        QR_benchmark_data<T> &all_data, 
+                                        RBKI_benchmark_data<T> &all_data, 
                                         RandBLAS::RNGState<RNG> &state) {
 
     RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.A, state);
-    std::fill(all_data.R.begin(), all_data.R.end(), 0.0);
-    std::fill(all_data.tau.begin(), all_data.tau.end(), 0.0);
-    std::fill(all_data.J.begin(), all_data.J.end(), 0);
+    std::fill(all_data.U.begin(), all_data.U.end(), 0.0);
+    std::fill(all_data.V.begin(), all_data.V.end(), 0.0);
+    std::fill(all_data.Sigma.begin(), all_data.Sigma.end(), 0.0);
 }
 
 template <typename T, typename RNG>
 static std::vector<long> call_all_algs(
     RandLAPACK::gen::mat_gen_info<T> m_info,
     int64_t numruns,
-    int64_t n,
-    QR_benchmark_data<T> &all_data,
+    int64_t k,
+    RBKI_benchmark_data<T> &all_data,
     RandBLAS::RNGState<RNG> &state) {
 
     auto m        = all_data.row;
+    auto n        = all_data.col;
     auto tol      = all_data.tolerance;
-    auto d_factor = all_data.sampling_factor;
 
     // Additional params setup.
-    RandLAPACK::CQRRPT<double, r123::Philox4x32> CQRRPT(true, true, tol);
-    CQRRPT.nnz = 4;
-    CQRRPT.num_threads = 48;
+    RandLAPACK::RBKI<double, r123::Philox4x32> RBKI(false, false, tol);
 
     // timing vars
-    long dur_cqrrpt     = 0;
-    long dur_geqp3      = 0;
-    long dur_geqr       = 0;
-    long dur_geqpt      = 0;
-    long dur_geqrf      = 0;
-    long dur_scholqr    = 0;
-    long t_cqrrpt_best  = 0;
-    long t_geqp3_best   = 0;
-    long t_geqr_best    = 0;
-    long t_geqpt_best   = 0;
-    long t_geqrf_best   = 0;
-    long t_scholqr_best = 0;
-    
+    long dur_rbki     = 0;
+    long dur_other    = 0;
+    long t_rbki_best  = 0;
+    long t_other_best = 0;
+
     // Making sure the states are unchanged
     auto state_gen = state;
     auto state_alg = state;
 
     for (int i = 0; i < numruns; ++i) {
         printf("Iteration %d start.\n", i);
-        // Testing GEQP3
-        auto start_geqp3 = high_resolution_clock::now();
-        lapack::geqp3(m, n, all_data.A.data(), m, all_data.J.data(), all_data.tau.data());
-        auto stop_geqp3 = high_resolution_clock::now();
-        dur_geqp3 = duration_cast<microseconds>(stop_geqp3 - start_geqp3).count();
+        
+        // Testing RBKI
+        auto start_rbki = high_resolution_clock::now();
+        RBKI.call(m, n, all_data.A.data(), m, k, all_data.U.data(), all_data.V.data(), all_data.Sigma.data(), state);
+        auto stop_rbki = high_resolution_clock::now();
+        dur_rbki = duration_cast<microseconds>(stop_rbki - start_rbki).count();
 
         state_gen = state;
         data_regen<T, RNG>(m_info, all_data, state_gen);
 
-        // Testing GEQRF
-        auto start_geqrf = high_resolution_clock::now();
-        lapack::geqrf(m, n, all_data.A.data(), m, all_data.tau.data());
-        auto stop_geqrf = high_resolution_clock::now();
-        dur_geqrf = duration_cast<microseconds>(stop_geqrf - start_geqrf).count();
-
-        state_gen = state;
-        data_regen<T, RNG>(m_info, all_data, state_gen);
-
-        // Testing CQRRPT
-        auto start_cqrrp = high_resolution_clock::now();
-        CQRRPT.call(m, n, all_data.A.data(), m, all_data.R.data(), n, all_data.J.data(), d_factor, state_alg);
-        auto stop_cqrrp = high_resolution_clock::now();
-        dur_cqrrpt = duration_cast<microseconds>(stop_cqrrp - start_cqrrp).count();
-
-        state_gen = state;
-        state_alg = state;
-        data_regen<T, RNG>(m_info, all_data, state_gen);
-
-        // Testing SCHOLQR3
-        auto start_scholqr = high_resolution_clock::now();
-        //--------------------------------------------------------------------------------------------------------------------------//
-        T norm_A = lapack::lange(Norm::Fro, m, n, all_data.A.data(), m);
-        T shift = 11 * std::numeric_limits<double>::epsilon() * n * std::pow(norm_A, 2);
-        blas::syrk(Layout::ColMajor, Uplo::Upper, Op::Trans, n, m, 1.0, all_data.A.data(), m, 0.0, all_data.R.data(), n);
-        for (int i = 0; i < n; ++i)
-            all_data.R[i * (n + 1)] += shift;
-        lapack::potrf(Uplo::Upper, n, all_data.R.data(), n);
-        blas::trsm(Layout::ColMajor, Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, m, n, 1.0, all_data.R.data(), n, all_data.A.data(), m);
-        // CholeskyQR2
-        blas::syrk(Layout::ColMajor, Uplo::Upper, Op::Trans, n, m, 1.0, all_data.A.data(), m, 0.0, all_data.R.data(), n);
-        lapack::potrf(Uplo::Upper, n, all_data.R.data(), n);
-        blas::trsm(Layout::ColMajor, Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, m, n, 1.0, all_data.R.data(), n, all_data.A.data(), m);
-        // CholeskyQR3
-        blas::syrk(Layout::ColMajor, Uplo::Upper, Op::Trans, n, m, 1.0, all_data.A.data(), m, 0.0, all_data.R.data(), n);
-        lapack::potrf(Uplo::Upper, n, all_data.R.data(), n);
-        blas::trsm(Layout::ColMajor, Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, m, n, 1.0, all_data.R.data(), n, all_data.A.data(), m);
-        //--------------------------------------------------------------------------------------------------------------------------//
-        auto stop_scholqr = high_resolution_clock::now();
-        dur_scholqr = duration_cast<microseconds>(stop_scholqr - start_scholqr).count();
-
-        auto state_gen = state;
-        data_regen<T, RNG>(m_info, all_data, state_gen);
-
-        // Testing GEQR + GEQPT
-        auto start_geqpt = high_resolution_clock::now();
-        auto start_geqr  = high_resolution_clock::now();
-#if !defined(__APPLE__)
-        // GEQR(A) part
-        lapack::geqr(m, n, all_data.A.data(), m,  all_data.tau.data(), -1);
-        int64_t tsize = (int64_t) all_data.tau[0]; 
-        all_data.tau.resize(tsize);
-        lapack::geqr(m, n, all_data.A.data(), m, all_data.tau.data(), tsize);
-#endif
-        auto stop_geqr = high_resolution_clock::now();
-        dur_geqr = duration_cast<microseconds>(stop_geqr - start_geqr).count();
-#if !defined(__APPLE__)
-        // GEQP3(R) part
-        lapack::lacpy(MatrixType::Upper, n, n, all_data.A.data(), m, all_data.R.data(), n);
-        lapack::geqp3(n, n, all_data.R.data(), n, all_data.J.data(), all_data.tau.data());
-#endif
-        auto stop_geqpt = high_resolution_clock::now();
-        dur_geqpt = duration_cast<microseconds>(stop_geqpt - start_geqpt).count();
+        // Testing Other
+        auto start_other = high_resolution_clock::now();
+        /// RIVAL ALGORITHM CALL
+        auto stop_other = high_resolution_clock::now();
+        dur_other = duration_cast<microseconds>(stop_other - start_other).count();
 
         state_gen = state;
         data_regen<T, RNG>(m_info, all_data, state_gen);
     
-        i == 0 ? t_cqrrpt_best  = dur_cqrrpt  : (dur_cqrrpt < t_cqrrpt_best)   ? t_cqrrpt_best = dur_cqrrpt   : NULL;
-        i == 0 ? t_geqpt_best   = dur_geqpt   : (dur_geqpt < t_geqpt_best)     ? t_geqpt_best = dur_geqpt     : NULL;
+        i == 0 ? t_rbki_best  = dur_rbki  : (dur_rbki < t_rbki_best)   ? t_rbki_best = dur_rbki   : NULL;
+        i == 0 ? t_other_best = dur_other : (dur_other < t_other_best) ? t_other_best = dur_other : NULL;
     }
 
-    std::vector<long> res{t_cqrrpt_best, t_geqpt_best};
+    std::vector<long> res{t_rbki_best, t_other_best};
 
     return res;
 }
 
 int main() {
     // Declare parameters
-    int64_t m           = std::pow(2, 17);
-    int64_t n_start     = std::pow(2, 9);
-    int64_t n_stop      = std::pow(2, 13);
-    double  d_factor    = 1.25;
+    int64_t m           = std::pow(10, 3);
+    int64_t n           = std::pow(10, 3);
+    int64_t k_start     = 100;
+    int64_t k_stop      = 100;
     double tol          = std::pow(std::numeric_limits<double>::epsilon(), 0.85);
     auto state          = RandBLAS::RNGState();
     auto state_constant = state;
     // Timing results
     std::vector<long> res;
     // Number of algorithm runs. We only record best times.
-    int64_t numruns = 1;
+    int64_t numruns = 5;
 
     // Allocate basic workspace
-    QR_benchmark_data<double> all_data(m, n_stop, tol, d_factor);
+    RBKI_benchmark_data<double> all_data(m, n, k_stop, tol);
+
     // Generate the input matrix - gaussian suffices for performance tests.
-    RandLAPACK::gen::mat_gen_info<double> m_info(m, n_stop, RandLAPACK::gen::gaussian);
+    RandLAPACK::gen::mat_gen_info<double> m_info(m, n, RandLAPACK::gen::gaussian);
     RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.A, state);
 
     // Declare a data file
-    std::fstream file("RBKI_speed_comp_"              + std::to_string(m)
-                                      + "_col_start_"    + std::to_string(n_start)
-                                      + "_col_stop_"     + std::to_string(n_stop)
-                                      + "_d_factor_"     + std::to_string(d_factor)
+    std::fstream file("RBKI_speed_comp_m_"          + std::to_string(m)
+                                      + "_n_"       + std::to_string(n)
+                                      + "_k_start_" + std::to_string(k_start)
+                                      + "_k_stop_"  + std::to_string(k_stop)
                                       + ".dat", std::fstream::app);
 
-    for (;n_start <= n_stop; n_start *= 2) {
-        res = call_all_algs<double, r123::Philox4x32>(m_info, numruns, n_start, all_data, state_constant);
-        file << res[0]  << ",  " << res[1]  << ",  " << res[2] << ",  " << res[3] << ",  " << res[4] << ",  " << res[5] << ",\n";
+    for (;k_start <= k_stop; k_start *= 2) {
+        res = call_all_algs<double, r123::Philox4x32>(m_info, numruns, k_start, all_data, state_constant);
+        file << res[0]  << ",  " << res[1]  << ",\n";
     }
 }
