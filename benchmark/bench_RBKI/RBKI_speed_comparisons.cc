c99a49a76e7d4c999aa1ed5847d247aace921214
TeachRaccooon <m.y.melnich@gmail.com>
2024-01-29 01:19:46 -0800

    Update

--- benchmark/bench_RBKI/RBKI_speed_comparisons.cc
+++ benchmark/bench_RBKI/RBKI_speed_comparisons.cc
@@ -16,18 +16,18 @@ struct RBKI_benchmark_data {
     std::vector<T> U;
     std::vector<T> V;
     std::vector<T> Sigma;
-    std::vector<T> Sigma_cpy_1;
-    std::vector<T> Sigma_cpy_2;
-    std::vector<T> Sigma_cpy_3;
+    std::vector<T> Sigma_cpy_RBKI;
+    std::vector<T> Sigma_cpy_SVD;
+    std::vector<T> Sigma_cpy_Other;
 
     RBKI_benchmark_data(int64_t m, int64_t n, int64_t k, T tol) :
     A(m * n, 0.0),
     U(m * n, 0.0),
     V(n * n, 0.0),
     Sigma(n, 0.0),
-    Sigma_cpy_1(n, 0.0),
-    Sigma_cpy_2(n, 0.0),
-    Sigma_cpy_3(n, 0.0)
+    Sigma_cpy_RBKI(n, 0.0),
+    Sigma_cpy_SVD(n, 0.0),
+    Sigma_cpy_Other(n, 0.0)
     {
         row = m;
         col = n;
@@ -40,9 +40,10 @@ struct RBKI_benchmark_data {
 template <typename T, typename RNG>
 static void data_regen(RandLAPACK::gen::mat_gen_info<T> m_info, 
                                         RBKI_benchmark_data<T> &all_data, 
-                                        RandBLAS::RNGState<RNG> &state) {
+                                        RandBLAS::RNGState<RNG> &state, int overwrite_A) {
 
-    RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.A.data(), state);
+    if (overwrite_A)
+        RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.A.data(), state);
     std::fill(all_data.U.begin(), all_data.U.end(), 0.0);
     std::fill(all_data.V.begin(), all_data.V.end(), 0.0);
     std::fill(all_data.Sigma.begin(), all_data.Sigma.end(), 0.0);
@@ -51,8 +52,6 @@ static void data_regen(RandLAPACK::gen::mat_gen_info<T> m_info,
 template <typename T>
 static void update_best_time(int iter, long &t_best, long &t_curr, T* S1, T* S2, int64_t k, long* break_in, long* break_out, int timing)
 {
-    // Can also do this is one line 
-    // i == 0 ? (void) (t_rbki_best = dur_rbki, accuracy_check ? blas::copy(n, all_data.Sigma.data(), 1, all_data.Sigma_cpy_1.data(), 1): (void) NULL) : (dur_rbki < t_rbki_best) ? ((void) (t_rbki_best = dur_rbki), accuracy_check ? blas::copy(n, all_data.Sigma.data(), 1, all_data.Sigma_cpy_1.data(), 1): (void) NULL) : (void) NULL;
     if (iter == 0 || t_curr < t_best) {
         t_best = t_curr;
         blas::copy(k, S1, 1, S2, 1);
@@ -60,14 +59,32 @@ static void update_best_time(int iter, long &t_best, long &t_curr, T* S1, T* S2,
     if (timing)
         blas::copy(13, break_out, 1, break_in, 1);
 }
-/*
-template <typename T>
-static void svd_error(T* U1, T* S1, T* VT1, T* U2, T* S2, T* VT2)
+
+template <typename T, typename RNG>
+static long run_svd(    
+    RandLAPACK::gen::mat_gen_info<T> m_info,
+    RBKI_benchmark_data<T> &all_data,
+    RandBLAS::RNGState<RNG> &state)
 {
-    blas::gemm(Layout::ColMajor, Op::NoTrans, Op::NoTrans, m, k, n, 1.0, A, m, Y_i, n, 0.0, X_i, m);
+
+    auto m        = all_data.row;
+    auto n        = all_data.col;
+    auto tol      = all_data.tolerance;
+
+    // Testing Other - SVD
+    auto start_svd = high_resolution_clock::now();
+    lapack::gesdd(Job::NoVec, m, n, all_data.A.data(), m, all_data.Sigma.data(), all_data.U.data(), m, all_data.V.data(), n);
+    auto stop_svd = high_resolution_clock::now();
+    long dur_svd = duration_cast<microseconds>(stop_svd - start_svd).count();
+
+    blas::copy(n, all_data.Sigma.data(), 1, all_data.Sigma_cpy_SVD.data(), 1);
+
+    auto state_gen = state;
+    data_regen<T, RNG>(m_info, all_data, state_gen, 1);
   
+    return dur_svd;
 }
-*/
+
 
 template <typename T, typename RNG>
 static void call_all_algs(
@@ -77,7 +94,8 @@ static void call_all_algs(
     int64_t num_krylov_iters,
     RBKI_benchmark_data<T> &all_data,
     RandBLAS::RNGState<RNG> &state,
-    std::string output_filename) {
+    std::string output_filename, 
+    long dur_svd) {
 
     int i, j;
     auto m        = all_data.row;
@@ -101,10 +119,8 @@ static void call_all_algs(
 
     // timing vars
     long dur_rbki        = 0;
-    long dur_other       = 0;
     long dur_lanchosz    = 0;
     long t_rbki_best     = 0;
-    long t_other_best    = 0;
     long t_lanchosz_best = 0;
 
     // Making sure the states are unchanged
@@ -125,10 +141,10 @@ static void call_all_algs(
         dur_lanchosz = duration_cast<microseconds>(stop_lanchosz - start_lanchosz).count();
 
         // Update best timing and save the singular values.
-        update_best_time<T>(i, t_lanchosz_best, dur_lanchosz, all_data.Sigma.data(), all_data.Sigma_cpy_3.data(), k_lanc, Lanc_timing_breakdown.data(), Lanchosz.times.data(), false);
+        //update_best_time<T>(i, t_lanchosz_best, dur_lanchosz, all_data.Sigma.data(), all_data.Sigma_cpy_Other.data(), k_lanc, Lanc_timing_breakdown.data(), Lanchosz.times.data(), false);
 
-        state_gen = state;
-        data_regen<T, RNG>(m_info, all_data, state_gen);
+        //state_gen = state;
+        //data_regen<T, RNG>(m_info, all_data, state_gen, 0);
         
         // Testing RBKI
         auto start_rbki = high_resolution_clock::now();
@@ -138,37 +154,23 @@ static void call_all_algs(
         dur_rbki = duration_cast<microseconds>(stop_rbki - start_rbki).count();
 
         // Update best timing and save the singular values.
-        update_best_time<T>(i, t_rbki_best, dur_rbki, all_data.Sigma.data(), all_data.Sigma_cpy_1.data(), k, RBKI_timing_breakdown.data(), RBKI.times.data(), time_subroutines);
+        update_best_time<T>(i, t_rbki_best, dur_rbki, all_data.Sigma.data(), all_data.Sigma_cpy_RBKI.data(), k, RBKI_timing_breakdown.data(), RBKI.times.data(), time_subroutines);
 
         state_gen = state;
-        data_regen<T, RNG>(m_info, all_data, state_gen);
-
-        // Testing Other - SVD
-        auto start_other = high_resolution_clock::now();
-        lapack::gesdd(Job::NoVec, m, n, all_data.A.data(), m, all_data.Sigma.data(), all_data.U.data(), m, all_data.V.data(), n);
-        auto stop_other = high_resolution_clock::now();
-        dur_other = duration_cast<microseconds>(stop_other - start_other).count();
-
-        blas::copy(n, all_data.Sigma.data(), 1, all_data.Sigma_cpy_2.data(), 1);
-
-        // Update best timing and save the singular values.
-        update_best_time<T>(i, t_other_best, dur_other, all_data.Sigma.data(), all_data.Sigma_cpy_2.data(), k, NULL, NULL, 0);
-
-        state_gen = state;
-        data_regen<T, RNG>(m_info, all_data, state_gen);
+        data_regen<T, RNG>(m_info, all_data, state_gen, 0);
     }
     
     for(j = 0; j < k; ++j)
-        all_data.Sigma_cpy_1[j] -= all_data.Sigma_cpy_2[j];
+        all_data.Sigma_cpy_RBKI[j] -= all_data.Sigma_cpy_SVD[j];
 
     for(j = 0; j < k_lanc; ++j) 
-        all_data.Sigma_cpy_3[j] -= all_data.Sigma_cpy_2[j];
+        all_data.Sigma_cpy_Other[j] -= all_data.Sigma_cpy_SVD[j];
 
-    norm_svd_k    = blas::nrm2(k,      all_data.Sigma_cpy_2.data(), 1);
-    norm_svd_lanc = blas::nrm2(k_lanc, all_data.Sigma_cpy_2.data(), 1);
+    norm_svd_k    = blas::nrm2(k,      all_data.Sigma_cpy_SVD.data(), 1);
+    norm_svd_lanc = blas::nrm2(k_lanc, all_data.Sigma_cpy_SVD.data(), 1);
     
-    err_rbki = blas::nrm2(k,      all_data.Sigma_cpy_1.data(), 1) / norm_svd_k;
-    err_lan  = blas::nrm2(k_lanc, all_data.Sigma_cpy_3.data(), 1) / norm_svd_lanc;
+    err_rbki = blas::nrm2(k,      all_data.Sigma_cpy_RBKI.data(), 1) / norm_svd_k;
+    err_lan  = blas::nrm2(k_lanc, all_data.Sigma_cpy_Other.data(), 1) / norm_svd_lanc;
 
     if (time_subroutines) {
         printf("\n\n/------------RBKI TIMING RESULTS BEGIN------------/\n");
@@ -205,11 +207,11 @@ static void call_all_algs(
     printf("||Sigma_ksvd - Sigma_rbki||_F / ||Sigma_ksvd||_F: %.16e\n", err_rbki);
     printf("||Sigma_ksvd - Sigma_lanc||_F / ||Sigma_lanc||_F: %.16e\n", err_lan);
 
-    printf("RBKI     is %f times faster that SVD.\n", (T) t_other_best / t_rbki_best);
-    printf("Lanchosz is %f times faster that SVD.\n", (T) t_other_best / t_lanchosz_best);
+    printf("RBKI     is %f times faster that SVD.\n", (T) dur_svd / t_rbki_best);
+    printf("Lanchosz is %f times faster that SVD.\n", (T) dur_svd / t_lanchosz_best);
 
     std::ofstream file(output_filename, std::ios::app);
-    file << k << ",  " << num_krylov_iters << ",  " << err_rbki << ",  " << err_lan <<  ",  " << t_rbki_best  << ",  " << t_other_best << ",  " << t_lanchosz_best  << ",\n";
+    file << k << ",  " << num_krylov_iters << ",  " << err_rbki << ",  " << err_lan <<  ",  " << t_rbki_best  << ",  " << dur_svd << ",  " << t_lanchosz_best  << ",\n";
 }
 
 int main(int argc, char *argv[]) {
@@ -264,46 +266,13 @@ int main(int argc, char *argv[]) {
                                       + "_num_krylov_iters_stop_"  + std::to_string(num_krylov_iters_stop)
                                       + ".dat"; 
 
+    // SVD run takes very long & is only needed once for all sizes
+    long dur_svd = run_svd<double, r123::Philox4x32>(m_info, all_data, state);
+
     for (;k_start <= k_stop; k_start *=2) {
         for (;num_krylov_iters_curr <= num_krylov_iters_stop; num_krylov_iters_curr *=2) {
-            call_all_algs<double, r123::Philox4x32>(m_info, numruns, k_start, num_krylov_iters_curr, all_data, state_constant, output_filename);
+            call_all_algs<double, r123::Philox4x32>(m_info, numruns, k_start, num_krylov_iters_curr, all_data, state_constant, output_filename, dur_svd);
         }
         num_krylov_iters_curr = num_krylov_iters_start;
     }
 }
-
-/*
-int main() {
-    // Declare parameters
-    int64_t m           = std::pow(10, 3);
-    int64_t n           = std::pow(10, 3);
-    int64_t k_start     = 100;
-    int64_t k_stop      = 100;
-    double tol          = std::pow(std::numeric_limits<double>::epsilon(), 0.85);
-    auto state          = RandBLAS::RNGState();
-    auto state_constant = state;
-    // Timing results
-    std::vector<long> res;
-    // Number of algorithm runs. We only record best times.
-    int64_t numruns = 5;
-
-    // Allocate basic workspace
-    RBKI_benchmark_data<double> all_data(m, n, k_stop, tol);
-
-    // Generate the input matrix - gaussian suffices for performance tests.
-    RandLAPACK::gen::mat_gen_info<double> m_info(m, n, RandLAPACK::gen::gaussian);
-    RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.A, state);
-
-    // Declare a data file
-    std::fstream file("RBKI_speed_comp_m_"          + std::to_string(m)
-                                      + "_n_"       + std::to_string(n)
-                                      + "_k_start_" + std::to_string(k_start)
-                                      + "_k_stop_"  + std::to_string(k_stop)
-                                      + ".dat", std::fstream::app);
-
-    for (;k_start <= k_stop; k_start *= 2) {
-        res = call_all_algs<double, r123::Philox4x32>(m_info, numruns, k_start, all_data, state_constant);
-        file << res[0]  << ",  " << res[1]  << ",\n";
-    }
-}
-*/
