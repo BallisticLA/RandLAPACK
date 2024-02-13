f86a09161ddd7fa8d94c347c40176720ae0ac033
TeachRaccooon <m.y.melnich@gmail.com>
2024-02-13 06:19:17 -0800

    Making sure GPU functions work

--- RandLAPACK/drivers/rl_cqrrpt_gpu.hh
+++ RandLAPACK/drivers/rl_cqrrpt_gpu.hh
@@ -199,9 +199,171 @@ int CQRRPT_GPU<T, RNG>::call(
     // Buffer for column pivoting.
     std::vector<int64_t> J_buf(n, 0);
 
-    T* A_hat_device;
-    cudaMalloc(&A_hat_device, d * n);
+    //T* A_hat_device;
+    //cudaMalloc(&A_hat_device, d * n);
+    //cudaMemcpy(A_hat, A_hat_device, d*n, cudaMemcpyHostToDevice);
 
+    if(this -> timing)
+        saso_t_start = high_resolution_clock::now();
+    /***********************************************************************************/
+    // I will avoid performing skething on a GPU for now
+
+    /// Generating a SASO
+    RandBLAS::SparseDist DS = {.n_rows = d, .n_cols = m, .vec_nnz = this->nnz};
+    RandBLAS::SparseSkOp<T, RNG> S(DS, state);
+    state = RandBLAS::fill_sparse(S);
+
+    /// Applying a SASO
+    RandBLAS::sketch_general(
+        Layout::ColMajor, Op::NoTrans, Op::NoTrans,
+        d, n, m, 1.0, S, 0, 0, A, lda, 0.0, A_hat, d
+    );
+
+    if(this -> timing) {
+        saso_t_stop = high_resolution_clock::now();
+        qrcp_t_start = high_resolution_clock::now();
+    }
+
+    /// Performing QRCP on a sketch
+    if(this->no_hqrrp) {
+        lapack::geqp3(d, n, A_hat, d, J, tau);
+    } else {
+        std::iota(J, &J[n], 1);
+        hqrrp(d, n, A_hat, d, J, tau, this->nb_alg, this->oversampling, this->panel_pivoting, this->use_cholqr, state, (T*) nullptr);
+    }
+
+    if(this -> timing) {
+        qrcp_t_stop = high_resolution_clock::now();
+        rank_reveal_t_start = high_resolution_clock::now();
+    }
+
+    /// Using naive rank estimation to ensure that R used for preconditioning is invertible.
+    /// The actual rank estimate k will be computed a posteriori. 
+    /// Using R[i,i] to approximate the i-th singular value of A_hat. 
+    /// Truncate at the largest i where R[i,i] / R[0,0] >= eps.
+    for(i = 0; i < n; ++i) {
+        if(std::abs(A_hat[i * d + i]) / std::abs(A_hat[0]) < eps_initial_rank_estimation) {
+            k = i;
+            break;
+        }
+    }
+    this->rank = k;
+
+    if(this -> timing)
+        rank_reveal_t_stop = high_resolution_clock::now();
+
+    // Allocating space for a preconditioner buffer.
+    T* R_sp  = ( T * ) calloc( k * k, sizeof( T ) );
+    /// Extracting a k by k upper-triangular R.
+    lapack::lacpy(MatrixType::Upper, k, k, A_hat, d, R_sp, k);
+    /// Extracting a k by n R representation (k by k upper-triangular, rest - general)
+    lapack::lacpy(MatrixType::Upper, k, k, A_hat, d, R, ldr);
+    lapack::lacpy(MatrixType::General, k, n - k, &A_hat[d * k], d, &R[n * k], ldr);
+
+    if(this -> timing)
+        a_mod_piv_t_start = high_resolution_clock::now();
+
+    // Swap k columns of A with pivots from J
+    blas::copy(n, J, 1, J_buf.data(), 1);
+    util::col_swap(m, n, k, A, lda, J_buf);
+
+    if(this -> timing) {
+        a_mod_piv_t_stop = high_resolution_clock::now();
+        a_mod_trsm_t_start = high_resolution_clock::now();
+    }
+
+    /******************************GPU REGION BEGIN*********************************/
+    // The reasons for using GPUs for this part only ar ethe following: 
+    // 1. There is no geqp3 available in any GPU linalg libraries. 
+    //    We could port HQRRP to GPUs, but that takes additional time.
+    // 2. There are no lacpy functions on any GPU linalg libraries. Those, however, 
+    //    can be substituted with blas::copy.
+    // 3. We do not have GPU-based skething support in RandBLAS at the moment.
+    //
+    // If the above points would be resolved, we may perform the entirety of CQRRPT on the device.
+    //
+    // Allocating device data & performing copies:
+    T* A_device;
+    T* R_sp_device;
+    blas::Queue blas_queue(0);
+    //lapack::Queue queue(0)
+    cudaMalloc(&A_device, m * n);
+    cudaMalloc(&R_sp_device, k * k);
+    cudaMemcpy(A, A_device, m * n, cudaMemcpyHostToDevice);
+    cudaMemcpy(R_sp, R_sp_device, k * k, cudaMemcpyHostToDevice);
+
+    char name [] = "A";
+    RandBLAS::util::print_colmaj(m, n, A, name);
+
+    // A_pre * R_sp = AP
+    blas::trsm(Layout::ColMajor, Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, m, k, 1.0, R_sp_device, k, A_device, lda, blas_queue);
+
+    cudaMemcpy(A_device, A, m * n, cudaMemcpyDeviceToHost);
+    cudaFree(A_device);
+    cudaFree(R_sp_device);
+
+    RandBLAS::util::print_colmaj(m, n, A, name);
+/*
+
+    if(this -> timing) {
+        a_mod_trsm_t_stop = high_resolution_clock::now();
+        cholqr_t_start = high_resolution_clock::now();
+    }
+
+    // Do Cholesky QR
+    blas::syrk(Layout::ColMajor, Uplo::Upper, Op::Trans, k, m, 1.0, A, lda, 0.0, R_sp, k);
+    lapack::potrf(Uplo::Upper, k, R_sp, k);
+
+    // Re-estimate rank after we have the R-factor form Cholesky QR.
+    // The strategy here is the same as in naive rank estimation.
+    // This also automatically takes care of any potentical failures in Cholesky factorization.
+    // Note that the diagonal of R_sp may not be sorted, so we need to keep the running max/min
+    // We expect the loss in the orthogonality of Q to be approximately equal to u * cond(R_sp)^2, where u is the unit roundoff for the numerical type T.
+    new_rank = k;
+    running_max = R_sp[0];
+    running_min = R_sp[0];
+    
+    for(i = 0; i < k; ++i) {
+        curr_entry = std::abs(R_sp[i * k + i]);
+        running_max = std::max(running_max, curr_entry);
+        running_min = std::min(running_min, curr_entry);
+        if(running_max / running_min >= std::sqrt(this->eps / std::numeric_limits<T>::epsilon())) {
+            new_rank = i - 1;
+            break;
+        }
+    }
+
+    blas::trsm(Layout::ColMajor, Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, m, new_rank, 1.0, R_sp, k, A, lda);
+
+    if(this -> timing)
+        cholqr_t_stop = high_resolution_clock::now();
+
+    // Get the final R-factor.
+    blas::trmm(Layout::ColMajor, Side::Left, Uplo::Upper, Op::NoTrans, Diag::NonUnit, new_rank, n, 1.0, R_sp, k, R, ldr);
+
+    // Set the rank parameter to the value comuted a posteriori.
+    this->rank = k;
+
+    if(this -> timing) {
+        saso_t_dur        = duration_cast<microseconds>(saso_t_stop - saso_t_start).count();
+        qrcp_t_dur        = duration_cast<microseconds>(qrcp_t_stop - qrcp_t_start).count();
+        rank_reveal_t_dur = duration_cast<microseconds>(rank_reveal_t_stop - rank_reveal_t_start).count();
+        a_mod_piv_t_dur   = duration_cast<microseconds>(a_mod_piv_t_stop - a_mod_piv_t_start).count();
+        a_mod_trsm_t_dur  = duration_cast<microseconds>(a_mod_trsm_t_stop - a_mod_trsm_t_start).count();
+        cholqr_t_dur      = duration_cast<microseconds>(cholqr_t_stop - cholqr_t_start).count();
+
+        total_t_stop = high_resolution_clock::now();
+        total_t_dur  = duration_cast<microseconds>(total_t_stop - total_t_start).count();
+        long t_rest  = total_t_dur - (saso_t_dur + qrcp_t_dur + rank_reveal_t_dur + cholqr_t_dur + a_mod_piv_t_dur + a_mod_trsm_t_dur);
+
+        // Fill the data vector
+        this -> times = {saso_t_dur, qrcp_t_dur, rank_reveal_t_dur, cholqr_t_dur, a_mod_piv_t_dur, a_mod_trsm_t_dur, t_rest, total_t_dur};
+    }
+
+    free(A_hat);
+    free(R_sp);
+    free(tau);
+    */
     return 0;
 }
 } // end namespace RandLAPACK
