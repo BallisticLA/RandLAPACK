6b60f3999394250ded26b7f527efd664c098ed71
TeachRaccooon <m.y.melnich@gmail.com>
2024-04-22 13:16:12 -0700

    Update

--- test/comps/test_util_gpu.cu
+++ test/comps/test_util_gpu.cu
@@ -34,14 +34,19 @@ class TestUtil : public ::testing::Test
         int64_t row;
         int64_t col;
         std::vector<T> A;
+        std::vector<T> B;
+        std::vector<T> C;
         std::vector<T> A_host_buffer;
         std::vector<int64_t> J;
         std::vector<int64_t> J_host_buffer;
         T* A_device;
+        T* B_device;
         T* J_device;
 
         ColSwpTestData(int64_t m, int64_t n) :
         A(m * n, 0.0),
+        B(m * n, 0.0),
+        C(m * n, 0.0),
         A_host_buffer(m * n, 0.0),
         J_host_buffer(n, 0.0),
         J(n, 0.0)
@@ -49,6 +54,7 @@ class TestUtil : public ::testing::Test
             row = m;
             col = n;
             cudaMalloc(&A_device, m * n * sizeof(T));
+            cudaMalloc(&B_device, m * n * sizeof(T));
             cudaMalloc(&J_device, n * sizeof(int64_t));
         }
     };
@@ -65,22 +71,26 @@ class TestUtil : public ::testing::Test
         char device_name [] = "device";
         RandBLAS::util::print_colmaj(m, n, all_data.A.data(), host_name);
         int i;
+        printf("Pivot vector on input.\n");
         for(i = 0; i < n; ++i)
             printf("%ld, ", all_data.J[i]);
         printf("\n");
 
         RandLAPACK::util::col_swap(m, n, n, all_data.A.data(), m, all_data.J);
+
         RandLAPACK::cuda_kernels::col_swap_gpu(m, n, n, all_data.A_device, m, all_data.J_device, strm);
         cudaMemcpy(all_data.A_host_buffer.data(), all_data.A_device, m * n * sizeof(T), cudaMemcpyDeviceToHost);
         cudaMemcpy(all_data.J_host_buffer.data(), all_data.J_device, n * sizeof(int64_t), cudaMemcpyDeviceToHost);
 
         // This prints correctly
         RandBLAS::util::print_colmaj(m, n, all_data.A_host_buffer.data(), device_name);
+        printf("Pivot vector on output - device.\n");
         for(i = 0; i < n; ++i)
             printf("%ld, ", all_data.J_host_buffer[i]);
         printf("\n");
 
         RandBLAS::util::print_colmaj(m, n, all_data.A.data(), host_name);
+        printf("Pivot vector on output - host.\n");
         for(i = 0; i < n; ++i)
             printf("%ld, ", all_data.J[i]);
         printf("\n");
@@ -91,14 +101,15 @@ class TestUtil : public ::testing::Test
         T norm_test = lapack::lange(Norm::Fro, m, n, all_data.A.data(), m);
         printf("Norm diff GPU CPU: %e\n", norm_test);
         ASSERT_NEAR(norm_test, 0.0, std::pow(std::numeric_limits<T>::epsilon(), 0.75));
+
     }
 
 };
 
 TEST_F(TestUtil, test_col_swp_gpu) {
     
-    int64_t m = 5;
-    int64_t n = 5;
+    int64_t m = 4;
+    int64_t n = 4;
     auto state = RandBLAS::RNGState();
     ColSwpTestData<double> all_data(m, n);
 
@@ -107,7 +118,7 @@ TEST_F(TestUtil, test_col_swp_gpu) {
     m_info.rank = n;
     m_info.exponent = 2.0;
     RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.A.data(), state);
-    //all_data.A = { 10.0, 20, 30.0, 40.0, 50.0 }; 
+    //all_data.A = { 1.0, 0, 0, 0, 0, 2.0, 0, 0, 0, 0, 3.0, 0, 0, 0, 0, 4.0}; 
     cudaMemcpy(all_data.A_device, all_data.A.data(), m * n * sizeof(double), cudaMemcpyHostToDevice);
     
     // Fill and randomly shuffle a vector
@@ -116,5 +127,39 @@ TEST_F(TestUtil, test_col_swp_gpu) {
     cudaMemcpy(all_data.J_device, all_data.J.data(), n * sizeof(int64_t), cudaMemcpyHostToDevice);
 
     test_col_swp_gpu<double>(all_data);
+}
+
+
+TEST_F(TestUtil, test_sum_gpu) {
+    
+    int64_t m = 4;
+    int64_t n = 4;
+    auto state = RandBLAS::RNGState();
+    ColSwpTestData<double> all_data(m, n);
+
+    RandLAPACK::gen::mat_gen_info<double> m_info(m, n, RandLAPACK::gen::polynomial);
+    m_info.cond_num = 2025;
+    m_info.rank = n;
+    m_info.exponent = 2.0;
+    RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.A.data(), state);
+    RandLAPACK::gen::mat_gen<double, r123::Philox4x32>(m_info, all_data.B.data(), state);
+    cudaMemcpy(all_data.A_device, all_data.A.data(), m * n * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(all_data.B_device, all_data.B.data(), m * n * sizeof(double), cudaMemcpyHostToDevice);
+
+    double* C_device;
+    cudaMalloc(&C_device, m * n * sizeof(double));
+    cudaStream_t strm = cudaStreamPerThread;
+
+    RandLAPACK::cuda_kernels::hadamard_product(all_data.A_device, all_data.B_device, C_device, n, m, m, strm);
+    cudaMemcpy(all_data.C.data(), C_device, m * n * sizeof(double), cudaMemcpyDeviceToHost);
+
+    char name1 [] = "A";
+    RandBLAS::util::print_colmaj(m, n, all_data.A.data(), name1);
+
+    char name2 [] = "C";
+    RandBLAS::util::print_colmaj(m, n, all_data.B.data(), name2);
+
+    char name [] = "C";
+    RandBLAS::util::print_colmaj(m, n, all_data.C.data(), name);
 }
 #endif
