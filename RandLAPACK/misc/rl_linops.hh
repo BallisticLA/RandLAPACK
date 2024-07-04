43ce8140cd63ad1b5e4ff1564b918e341e3c0c47
Riley Murray <rjmurr@sandia.gov>
2024-07-04 09:41:41 -0400

    lockstep and block PCG

--- RandLAPACK/misc/rl_linops.hh
+++ RandLAPACK/misc/rl_linops.hh
@@ -47,17 +47,17 @@ struct SymmetricLinearOperator {
 template <typename T>
 struct ExplicitSymLinOp : public SymmetricLinearOperator<T> {
 
-    const Uplo uplo;
+    const blas::Uplo uplo;
     const T* A_buff;
     const int64_t lda;
-    const Layout buff_layout;
+    const blas::Layout buff_layout;
 
     ExplicitSymLinOp(
         int64_t m,
-        Uplo uplo,
+        blas::Uplo uplo,
         const T* A_buff,
         int64_t lda,
-        Layout buff_layout
+        blas::Layout buff_layout
     ) : SymmetricLinearOperator<T>(m), uplo(uplo), A_buff(A_buff), lda(lda), buff_layout(buff_layout) {};
 
     // Note: the "layout" parameter here is interpreted for (B and C).
@@ -65,7 +65,7 @@ struct ExplicitSymLinOp : public SymmetricLinearOperator<T> {
     // parameters to blas::symm to reconcile the different layouts of
     // A vs (B, C).
     void operator()(
-        Layout layout,
+        blas::Layout layout,
         int64_t n,
         T alpha,
         T* const B,
@@ -78,7 +78,7 @@ struct ExplicitSymLinOp : public SymmetricLinearOperator<T> {
         randblas_require(ldc >= this->m);
         auto blas_call_uplo = this->uplo;
         if (layout != this->buff_layout)
-            blas_call_uplo = (this->uplo == Uplo::Upper) ? Uplo::Lower : Uplo::Upper;
+            blas_call_uplo = (this->uplo == blas::Uplo::Upper) ? blas::Uplo::Lower : blas::Uplo::Upper;
         // Reading the "blas_call_uplo" triangle of "this->A_buff" in "layout" order is the same
         // as reading the "this->uplo" triangle of "this->A_buff" in "this->buff_layout" order.
         blas::symm(
@@ -88,5 +88,50 @@ struct ExplicitSymLinOp : public SymmetricLinearOperator<T> {
     };
 };
 
+
+// template <typename T>
+// struct RegularizedSymLinOp  {
+//
+//     const int64_t m;
+//     std::vector<T> regs;
+//
+//     RegularizedSymLinOp(int64_t m, std::vector<T> &regs) : m(m), regs(regs) {};
+//
+//     virtual void operator()(T alpha, T* const B, int64_t ldb, T beta, T* C, int64_t ldc) = 0;
+//
+// };
+
+template <typename T>
+struct RegExplicitSymLinOp : public SymmetricLinearOperator<T> {
+
+    const T* A_buff;
+    const int64_t lda;
+    std::vector<T> regs;
+    static const blas::Uplo uplo = blas::Uplo::Upper;
+    static const blas::Layout buff_layout = blas::Layout::ColMajor;
+
+    RegExplicitSymLinOp(
+        int64_t m, const T* A_buff, int64_t lda, std::vector<T> &regs
+    ) : SymmetricLinearOperator<T>(m), A_buff(A_buff), lda(lda), regs(regs) {
+        randblas_require(lda >= m);
+    };
+
+    void operator()(blas::Layout layout, int64_t n, T alpha, T* const B, int64_t ldb, T beta, T* C, int64_t ldc) {
+        randblas_require(layout == this->buff_layout);
+        randblas_require(ldb >= this->m);
+        randblas_require(ldc >= this->m);
+        blas::symm(layout, blas::Side::Left, this->uplo, this->m, n, alpha, this->A_buff, this->lda, B, ldb, beta, C, ldc);
+        int64_t num_regs = this->regs.size();
+        if (num_regs != 1)
+            randblas_require(n == num_regs);
+        T* regsp = regs.data();
+        for (int64_t i = 0; i < n; ++i) {
+            T coeff =  alpha * regsp[std::min(i, num_regs - 1)];
+            blas::axpy(this->m, coeff, B + i*ldb, 1, C +  i*ldc, 1);
+        }
+        return;
+    };
+
+};
 
 } // end namespace RandLAPACK
--- test/comps/test_determiter.cc
+++ test/comps/test_determiter.cc
@@ -1,13 +1,21 @@
 #include "RandLAPACK.hh"
 #include "rl_blaspp.hh"
+#include "/Users/rjmurr/Documents/randnla/RandLAPACK/RandBLAS/test/comparison.hh"
 
 #include <RandBLAS.hh>
 #include <math.h>
 #include <gtest/gtest.h>
 
+template <typename T>
+std::vector<T> eye(int64_t n) {
+    std::vector<T> A(n * n, 0.0);
+    for (int i = 0; i < n; ++i)
+        A[i + n*i] = 1.0;
+    return A;
+}
 
-class TestDetermiterOLS : public ::testing::Test
-{
+
+class TestDetermiterOLS : public ::testing::Test {
     protected:
         int64_t m = 201;
         int64_t n = 12;
@@ -17,8 +25,7 @@ class TestDetermiterOLS : public ::testing::Test
 
     virtual void TearDown() {};
 
-    virtual void run(uint64_t key_index)
-    {   
+    virtual void run(uint64_t key_index) {   
         std::vector<double> A(m * n);
         RandBLAS::util::genmat(m, n, A.data(), keys[key_index]);
         
@@ -39,9 +46,10 @@ class TestDetermiterOLS : public ::testing::Test
         double delta = 0.1;
 	    double tol = 1e-8;
 
-        RandLAPACK::pcg(
+        RandLAPACK::pcg_saddle(
             m, n, A.data(), m, b.data(), c.data(), delta,
-            resid_vec, tol, n, M.data(), n, x0.data(), x.data(), y.data());
+            resid_vec, tol, n, M.data(), n, x0.data(), x.data(), y.data()
+        );
         
 
         int64_t iter_count = 0;
@@ -63,4 +71,135 @@ TEST_F(TestDetermiterOLS, Trivial) {
     for (int64_t k_idx : {0, 1, 2}) {
         run(k_idx);
     }
+}
+
+
+class TestDetermiterLockBlockPCG : public ::testing::Test {
+    protected:
+    
+    virtual void SetUp() {};
+
+    virtual void TearDown() {};
+
+    template <typename T>
+    void run_simple_block(int64_t m, int64_t s, T coeff, uint32_t seed) {   
+        using std::vector;
+        auto layout = blas::Layout::ColMajor;
+        vector<T> G_buff(m*m, 0.0);
+        vector<T> H(m*s, 0.0);
+        randblas_require((int64_t) H.size() == m*s);
+        vector<T> X_star(m*s, 0.0);
+        vector<T> X_init(m*s, 0.0);
+        RandBLAS::RNGState state0(seed);
+        vector<T> temp(2*m*m);
+        auto D = RandBLAS::DenseDist {2*m, m, RandBLAS::DenseDistName::Gaussian};
+        auto out1 = RandBLAS::fill_dense(D, temp.data(), state0);
+        auto state1 = std::get<1>(out1);
+        blas::syrk(layout, blas::Uplo::Upper, blas::Op::Trans, m, 2*m, 1.0, temp.data(), 2*m, 0.0, G_buff.data(), m);
+
+        vector<T> regs(1, coeff);
+        RandLAPACK::RegExplicitSymLinOp G(m, G_buff.data(), m, regs);
+        RandBLAS::DenseDist DX_star {m, s, RandBLAS::DenseDistName::Gaussian};
+        auto Xsd = X_star.data();
+        auto out2 = RandBLAS::fill_dense(DX_star, Xsd, state1);
+        auto state2 = std::get<1>(out2);
+        G(layout, s, 1.0, X_star.data(), m, 0.0, H.data(), m);
+
+        RandLAPACK::StatefulFrobeniusNorm<T> seminorm{};
+
+        auto I_buff = eye<T>(m);
+        vector<T> zeros(1, 0.0);
+        RandLAPACK::RegExplicitSymLinOp I(m, I_buff.data(), m, zeros);
+
+        T tol = 100*std::numeric_limits<T>::epsilon();
+        RandLAPACK::lockorblock_pcg(G, H, tol, m, I, seminorm, X_init, true);
+
+        T tol_scale = std::sqrt((T)m);
+        T atol = tol_scale * std::pow(std::numeric_limits<T>::epsilon(), 0.5);
+        T rtol = tol_scale * atol;
+        test::comparison::buffs_approx_equal(X_init.data(), X_star.data(), m * s,
+            __PRETTY_FUNCTION__, __FILE__, __LINE__, atol, rtol
+        );
+        return;
+    }
+
+    virtual void run_simple_lockstep(int64_t m, int64_t s, uint32_t seed) {  
+        using T = double;
+        randblas_require(s <= 4);
+        using std::vector;
+        vector<T> reg_coeffs{};
+        reg_coeffs.push_back(100);
+        if (s > 1)
+            reg_coeffs.push_back(7);
+        if (s > 2)
+            reg_coeffs.push_back(0.1);
+        if (s > 3)
+            reg_coeffs.push_back(0.5483);
+        auto layout = blas::Layout::ColMajor;
+        vector<T> G_buff(m*m, 0.0);
+        vector<T> H(m*s, 0.0);
+        randblas_require((int64_t) H.size() == m*s);
+        vector<T> X_star(m*s, 0.0);
+        vector<T> X_init(m*s, 0.0);
+        RandBLAS::RNGState state0(seed);
+        vector<T> temp(2*m*m);
+        
+        auto D = RandBLAS::DenseDist {2*m, m, RandBLAS::DenseDistName::Gaussian};
+        auto out1 = RandBLAS::fill_dense(D, temp.data(), state0);
+        auto state1 = std::get<1>(out1);
+        blas::syrk(layout, blas::Uplo::Upper, blas::Op::Trans, m, 2*m, 1.0, temp.data(), 2*m, 0.0, G_buff.data(), m);
+
+        vector<T> regs(reg_coeffs);
+        RandLAPACK::RegExplicitSymLinOp G(m, G_buff.data(), m, regs);
+        RandBLAS::DenseDist DX_star {m, s, RandBLAS::DenseDistName::Gaussian};
+        auto Xsd = X_star.data();
+        auto out2 = RandBLAS::fill_dense(DX_star, Xsd, state1);
+        auto state2 = std::get<1>(out2);
+        G(layout, s, 1.0, X_star.data(), m, 0.0, H.data(), m);
+
+        RandLAPACK::StatefulFrobeniusNorm<T> seminorm{};
+
+        auto I_buff = eye<T>(m);
+        vector<T> zeros(s, 0.0);
+        RandLAPACK::RegExplicitSymLinOp I(m, I_buff.data(), m, zeros);
+
+        T tol = 100*std::numeric_limits<T>::epsilon();
+        RandLAPACK::lockorblock_pcg(G, H, tol, m, I, seminorm, X_init, true);
+
+        T tol_scale = std::sqrt((T)m);
+        T atol = tol_scale * std::pow(std::numeric_limits<T>::epsilon(), 0.5);
+        T rtol = tol_scale * atol;
+        test::comparison::buffs_approx_equal(X_init.data(), X_star.data(), m * s,
+            __PRETTY_FUNCTION__, __FILE__, __LINE__, atol, rtol
+        );
+        return;
+    }
+};
+
+
+TEST_F(TestDetermiterLockBlockPCG, test_run_simple_block_5_1) {
+    run_simple_block<double>(5, 1, 0.5, 1997);
+}
+
+TEST_F(TestDetermiterLockBlockPCG, test_run_simple_block_6_2) {
+    run_simple_block<double>(6, 2, 0.5, 1997);
+}
+
+TEST_F(TestDetermiterLockBlockPCG, test_run_simple_block_5_4) {
+    run_simple_block<double>(5, 4, 0.5, 1997);
+}
+
+TEST_F(TestDetermiterLockBlockPCG, test_run_simple_lockstep_5_1) {
+    run_simple_lockstep(5, 1, 1997);
+    run_simple_lockstep(5, 1, 2024);
+}
+
+TEST_F(TestDetermiterLockBlockPCG, test_run_simple_lockstep_6_2) {
+    run_simple_lockstep(6, 2, 1997);
+    run_simple_lockstep(6, 2, 2024);
+}
+
+TEST_F(TestDetermiterLockBlockPCG, test_run_simple_lockstep_5_4) {
+    run_simple_lockstep(5, 4, 1997);
+    run_simple_lockstep(5, 4, 2024);
 }
