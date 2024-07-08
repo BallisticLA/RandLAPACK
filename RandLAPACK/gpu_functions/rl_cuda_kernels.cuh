fd55291922ddc1654a5c96ed9dce1db636e40c28
TeachRaccooon <m.y.melnich@gmail.com>
2024-07-08 07:30:15 -0700

    GPU kernels are finally set up properly

--- RandLAPACK/gpu_functions/rl_cuda_kernels.cuh
+++ RandLAPACK/gpu_functions/rl_cuda_kernels.cuh
@@ -16,20 +16,89 @@ namespace RandLAPACK::cuda_kernels {
  */
 
 template <typename T>
-__global__ void __launch_bounds__(128) copy_gpu(int64_t n, T const* src, int64_t incr_src, T* dest, int64_t incr_dest) {
+__global__ void __launch_bounds__(128) copy_gpu(
+    int64_t n, 
+    T const* src, 
+    int64_t incsrc, 
+    T* dest, 
+    int64_t incdest) {
     int64_t const id = (int64_t)blockDim.x * blockIdx.x + threadIdx.x;
     if (id < n) {
-        dest[id * incr_dest] = src[id * incr_src];
+        dest[id * incdest] = src[id * incsrc];
     }
 }
 
 template <typename T>
-__global__ void __launch_bounds__(128) swap_gpu(T* a, int64_t incr_a, T* b, int64_t n, int64_t incr_b) {
+__device__ void copy_gpu_device(
+    int64_t n,
+    T const* src,
+    int64_t incsrc,
+    T* dest,
+    int64_t incdest) {
     int64_t const id = (int64_t)blockDim.x * blockIdx.x + threadIdx.x;
     if (id < n) {
-        T const v{a[id * incr_a]};
-        a[id * incr_a] = b[id * incr_b];
-        b[id * incr_b] = v;
+        dest[id * incdest] = src[id * incsrc];
+    }
+}
+
+template <typename T>
+__global__ void __launch_bounds__(128) swap_gpu(
+    T* a, 
+    int64_t inca, 
+    T* b, 
+    int64_t n, 
+    int64_t incb) {
+    int64_t const id = (int64_t)blockDim.x * blockIdx.x + threadIdx.x;
+    if (id < n) {
+        T const v{a[id * inca]};
+        a[id * inca] = b[id * incb];
+        b[id * incb] = v;
+    }
+}
+
+template <typename T>
+__global__  void __launch_bounds__(128) axpy_gpu(
+    int64_t n, 
+    T alpha, 
+    T *x, 
+    int64_t incx, 
+    T *y, 
+    int64_t incy) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        y[i * incy] = alpha * x[i * incx] + y[i * incy];
+    }
+}
+
+template <typename T>
+__device__  void axpy_gpu_device(
+    int64_t n, 
+    T alpha, 
+    T *x, 
+    int64_t incx, 
+    T *y, 
+    int64_t incy) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        y[i * incy] = alpha * x[i * incx] + y[i * incy];
+    }
+}
+
+template <typename T>
+__global__  void __launch_bounds__(128) ger_gpu(
+    int64_t m, 
+    int64_t n, 
+    T alpha, 
+    T *x, 
+    int64_t incx, 
+    T *y, 
+    int64_t incy, 
+    T* A, 
+    int64_t lda
+) {
+    #pragma unroll
+    for(int i = 0; i < n; ++i) {
+        axpy_gpu_device(m, alpha * y[i * incy], x, incx, &A[i * lda], 1);
     }
 }
 
@@ -37,8 +106,11 @@ __global__ void __launch_bounds__(128) swap_gpu(T* a, int64_t incr_a, T* b, int6
 /// idx array is to modified modified ONLY within the scope of this function.
 template <typename T>
 __global__ void __launch_bounds__(128) col_swap_gpu(
-    int64_t m, int64_t n, int64_t k,
-    T* A, int64_t lda,
+    int64_t m, 
+    int64_t n, 
+    int64_t k,
+    T* A, 
+    int64_t lda,
     int64_t const* idx
 ) {
     extern __shared__ int64_t local_idx[];
@@ -78,34 +150,49 @@ void transposition_gpu(
     int64_t lda,
     T* AT,
     int64_t ldat,
-    int copy_upper_triangle,
-    int64_t num_blocks,
-    int64_t threadsPerBlock
+    int copy_upper_triangle
 ) {
     if (copy_upper_triangle) {
         // Only transposing the upper-triangular portion of the original
-        for(int64_t i = threadIdx.x; i < n; i += blockDim.x)
-            copy_gpu<<<num_blocks, threadsPerBlock>>>(i + 1, &A[i * lda], 1, &AT[i], ldat);
-            __syncthreads();
+        #pragma unroll
+        for(int64_t i = 0; i < n; ++i){
+            copy_gpu_device(i + 1, &A[i * lda], 1, &AT[i], ldat);
+        }
     } else {
-        for(int64_t i = threadIdx.x; i < n; i += blockDim.x)
-            copy_gpu<<<num_blocks, threadsPerBlock>>>(m, &A[i * lda], 1, &AT[i], ldat);
-            __syncthreads();
+        #pragma unroll
+        for(int64_t i = 0; i < n; ++i){
+            copy_gpu_device(m, &A[i * lda], 1, &AT[i], ldat);
+        }
     }
 }
-
 #endif
-
 
 template <typename T>
-void copy_gpu(cudaStream_t stream, int64_t n, T const* src, int64_t incr_src, T* dest, int64_t incr_dest) {
-    
+void ger_gpu(
+    cudaStream_t stream, 
+    int64_t m, 
+    int64_t n, 
+    T alpha, 
+    T *x, 
+    int64_t incx, 
+    T *y, 
+    int64_t incy, 
+    T* A, 
+    int64_t lda
+) {
+
 #ifdef USE_CUDA
     constexpr int threadsPerBlock{128};
-    int64_t num_blocks{(n + threadsPerBlock - 1) / threadsPerBlock};
-    copy_gpu<<<num_blocks, threadsPerBlock, 0, stream>>>(n, src, incr_src, dest, incr_dest);
-    cudaStreamSynchronize(stream);
+    int64_t num_blocks_ger{(m + threadsPerBlock - 1) / threadsPerBlock};
+    ger_gpu<<<num_blocks_ger, threadsPerBlock, 0, stream>>>(m, n, alpha, x, incx, y, incy, A, lda);
 #endif
+  
+    cudaError_t ierr = cudaGetLastError();
+    if (ierr != cudaSuccess)
+    {
+        BPCG_ERROR("Failed to launch ger_gpu. " << cudaGetErrorString(ierr))
+        abort();
+    }
 }
 
 /// Positions columns of A in accordance with idx vector of length k.
@@ -148,9 +235,8 @@ void transposition_gpu(
 ) {
 #ifdef USE_CUDA
     constexpr int threadsPerBlock{128};
-    int64_t num_blocks_copy{(m + threadsPerBlock - 1) / threadsPerBlock};
-    int64_t num_blocks_transposition{(n + threadsPerBlock - 1) / threadsPerBlock};
-    transposition_gpu<<<num_blocks_transposition, threadsPerBlock, 0, stream>>>(m, n, A, lda, AT, ldat, copy_upper_triangle, num_blocks_copy, threadsPerBlock);
+    int64_t num_blocks{(m + threadsPerBlock - 1) / threadsPerBlock};
+    transposition_gpu<<<num_blocks, threadsPerBlock, 0, stream>>>(m, n, A, lda, AT, ldat, copy_upper_triangle);
 #endif
     cudaError_t ierr = cudaGetLastError();
     if (ierr != cudaSuccess)
