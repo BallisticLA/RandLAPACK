4c556aa091084b00fbebcdb3411304e3a92f2576
Riley Murray <rjmurr@sandia.gov>
2024-07-24 10:00:58 -0400

    check in

--- RandLAPACK/drivers/rl_krillx.hh
+++ RandLAPACK/drivers/rl_krillx.hh
@@ -12,6 +12,16 @@
 #include <limits>
 #include <vector>
 
+/**
+ * 
+ * TODO:
+ *  (1) finish and test krill_restricted_rpchol
+ *  (2) write and test a krill_restricted function that accepts the centers as inputs
+ *      in advance.
+ *  (3) See also, rl_preconditioners.hh
+ * 
+ */
+
 namespace RandLAPACK {
 
 /**
@@ -52,6 +62,93 @@ STATE krill_full_rpchol(
     return state;
 }
 
+/**
+ * We start with a regularized kernel linear operator G and target data H.
+ * We use "K" to denote the unregularized version of G, which can be accessed
+ * by calling G.set_eval_includes_reg(false);
+ * 
+ * If G.regs.size() == 1, then the nominal KRR problem reduces to computing
+ * 
+ *     (K + G.regs[0] * I) X = H.       (*)
+ * 
+ * If G.regs.size() > 1, then KRR is nominally about solving the independent
+ * collection of problems
+ * 
+ *      (K + mu_i * I) x_i = h_i,       (**)
+ * 
+ * where K is the unregularized version of G, mu_i = G.regs[i], and x_i, h_i
+ * are the i-th columns of X and H respectively. In this situation we need
+ * H to have exactly G.regs.size() columns.
+ *      
+ * This function produces __approximate__ solutions to KRR problems. It does so
+ * by finding a set of indices for which
+ * 
+ *      K_hat = K(:,inds) * inv(K(inds, inds)) * K(inds, :) 
+ * 
+ * is a good low-rank approximation of K. We spend O(n*k^2) arithmetic operations and
+ * O(n*k_ evaluations of K(i,j) to get our hands on "inds" and a factored representation
+ * of K_hat.
+ * 
+ * Given inds, we turn our attention to solving the problem
+ * 
+ *      min{ || K(:,inds) x - H ||_2^2 + mu || sqrtm(K(inds, inds)) x ||_2^2 : x  }.
+ *      
+ * We don't store K(:,inds) explicitly. Instead, we have access to a matrix V where
+ * 
+ *      (i)   K_hat = VV',
+ *      (ii)  V(inds,:)V(inds,:)' = K(inds, inds), and
+ *      (iii) V*V(inds,:)' = K_hat(:,inds) = K(:, inds).
+ * 
+ * If we abbreviate M := V(inds, :), then the restricted KRR problem can be framed as 
+ * 
+ *      min{ || V M' x - H ||_2^2 + mu || M' X ||_2^2  :  x  }.
+ * 
+ * We approach this by a change of basis, solving problems like
+ * 
+ *      min{ ||V y - H||_2^2 + mus || y ||_2^2 : y }        (***)
+ * 
+ *  and then returning x = inv(M') y.
+ * 
+ * Note that since we spend O(n*k^2) time getting our hands on V and inds, it would be
+ * reasonable to spend O(n*k^2) additional time to solve (***) by a direct method.
+ * However, it is easy enough to reduce the cost of solving (***) to o(n*k^2)
+ * (that is, little-o of n*k^2) by a sketch and precondition approach. 
+ *
+ */
+template <typename T, typename FUNC, typename SEMINORM, typename STATE>
+STATE krill_restricted_rpchol(
+    int64_t n, FUNC &G, vector<T> &H, vector<T> &X, T tol,
+    STATE state, SEMINORM seminorm, int64_t rpchol_block_size = -1, int64_t max_iters = 20, int64_t k = -1
+) {
+    // NOTE: on entry, X is n-by-s for some integer s. That's way bigger than it needs to be, since the
+    // solution we return can be written down with k*s nonzeros plus k indices to indicate which rows of X
+    // are nonzero.
+    vector<T> V(n*k, 0.0);
+    vector<T> eigvals(k, 0.0);
+    G.set_eval_includes_reg(false);
+
+    vector<int64_t> inds(k, -1);
+    state = rp_cholesky(n, G, k, inds.data(), V.data(), rpchol_block_size, state);
+    inds.resize(k);
+    // ^ VV' defines a rank-k Nystrom approximation of G. The approximation satisfies
+    //
+    //          VV' = G(:,inds) * inv(G(inds, inds)) * G(inds, :) 
+    //   and
+    //          (VV')(inds, inds) = G(inds, inds).
+    //
+    //   That second identity can be written as MM' = G(inds, inds) for M = V(inds, :).
+    //
+
+
+    vector<T> M(k * k);
+    _rpchol_imp::pack_selected_rows(blas::Layout::ColMajor, n, k, V.data(), inds, M.data());
+    //
+    //
+    //
+
+    linops::SpectralPrecond<T> invP(n);
+    invP.prep(V, eigvals, mus, ell);
+}
 
 // template <typename T, typename FUNC, typename STATE>
 // STATE krill_block(
