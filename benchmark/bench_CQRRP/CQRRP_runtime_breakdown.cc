1b255eb526aa0fea9a85ad7f4559ea21a9c684f0
rileyjmurray <rjmurr@sandia.gov>
2024-10-05 19:24:06 -0700

    EPYC-9354P benchmarks

--- benchmark/bench_CQRRP/CQRRP_runtime_breakdown.cc
+++ benchmark/bench_CQRRP/CQRRP_runtime_breakdown.cc
@@ -112,15 +112,15 @@ int main(int argc, char *argv[]) {
     int64_t m          = std::stol(size);
     int64_t n          = std::stol(size);
     double  d_factor   = 1.0;
-    int64_t b_sz_start = 32;
-    int64_t b_sz_end   = 2048;
+    int64_t b_sz_start = 256;
+    int64_t b_sz_end   = 1024;
     double tol         = std::pow(std::numeric_limits<double>::epsilon(), 0.85);
     auto state         = RandBLAS::RNGState<r123::Philox4x32>();
     auto state_constant = state;
     // Timing results
     std::vector<long> res;
     // Number of algorithm runs. We only record best times.
-    int64_t numruns = 5;
+    int64_t numruns = 3;
 
     // Allocate basic workspace
     QR_speed_benchmark_data<double> all_data(m, n, tol, d_factor);
