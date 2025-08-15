404cfa9022726780acc59a171aee70ffa3ec2566
Tanya Tafolla <ttafolla@ucmerced.edu>
2025-08-14 18:44:03 -0700

    my additions with setup_work structure; this initializes the augmented work_matrix, computes qr on the blocks (Mitchell), computes the R-score (Mitchell), pivots the highest scoring block to front, and perform an unpivoted qr on the original A matrix (not augmented)

--- prototyping/qrbbrp_qp3_demo.cc
+++ prototyping/qrbbrp_qp3_demo.cc
@@ -6,20 +6,153 @@
 #include <RandBLAS.hh>
 #include <vector>
 
+//Riley's function
+void set_J(int64_t* J, int64_t len_J, int64_t b, int64_t chosen_block) {
+    std::iota(J, J + len_J, 1);
+    // ^ J = {1, 2, ..., len_J}
+    std::swap_ranges(J, J + b, J + b*chosen_block);
+    return;
+}
+
+//Riley's swap function
+template <typename T>
+void swap_cols(int64_t m, int64_t n, T* A, int64_t lda, int64_t b, int64_t chosen_block) {
+    for (int64_t i = 0; i < b; ++i) {
+        T* ai_current = A + lda*i;
+        T* ai_desired = A + lda*(chosen_block*b + i);
+        std::swap_ranges(ai_current, ai_current + m, ai_desired);
+    }
+}
+
+//struc for setting up work matrix
+template<typename T> 
+struct setup_work {
+
+    std::vector<T> work_matrix;
+    int64_t num_blocks;
+    int64_t block_size;
+
+    void reserve(int64_t rows, int64_t cols) { work_matrix.resize((rows+block_size)*cols, 0.0); }
+    void free() { return; }
+    void operator()(int64_t rows, int64_t cols, T* A, int64_t _lda, int64_t* J) {
+	   
+    int cols_per_block = cols / num_blocks;
+
+    T* diagsR = new T[cols_per_block]();
+    T* v      = new T[num_blocks]{0.0};
+    T* tau    = new T[cols_per_block]();
+
+    T* extra_arr = new T[cols](); //for geqrf
+
+    //1. initialize work_matrix to 0
+    for (int i = 0; i < cols*(rows+block_size); i++) {
+       work_matrix[i] = 0.0;
+    }
+
+    //2. set up augmented work matrix
+    int indx_work, indx_A;
+    for (int i = 0; i < cols; i++) {
+       for (int j = 0; j < rows; j++){
+          indx_A    = j + i*rows;
+          indx_work = indx_A + block_size*i;
+          
+	  work_matrix[indx_work] = A[indx_A];
+        }
+
+       //then set the bottom idenity
+       //rem+1
+       indx_work += (i % cols_per_block) + 1;
+       work_matrix[indx_work] = 1.0;
+    }	
+          
+
+    //3. use mitchell's code to compute pivot scores of blocks
+    for (int i = 0; i < num_blocks; ++i) {
+       // Call the QR function for each block
+       T* A_block = &A[i * cols_per_block * rows];
+       lapack::geqrf(rows, cols_per_block, A_block, rows, tau);
+
+       // Extract the diagonal elements of R
+       for (int j = 0; j < cols_per_block; ++j) {
+         diagsR[j] = A_block[j  * (rows + 1)];
+       } 
+
+       // Store the sum log |diagsR[j] | for each block in v[j]
+       for (int j = 0; j < cols_per_block; ++j) {
+           v[i] += std::log(std::abs(diagsR[j]));
+       }
+
+     }
+
+    //4. swap blocks of original matrix A
+    //find max of v
+
+    float max_score = v[0];
+    int idx_of_max  = 0;
+    for (int i = 1; i < num_blocks; i++){
+       if (v[i] > max_score){
+	  max_score = v[i];
+	  idx_of_max = i;
+       }
+    }
+
+    swap_cols(rows, cols, A, rows, cols_per_block, idx_of_max);
+
+    //6. unpivoted QR on A matrix
+    //original matrix, not the augmented
+    lapack::geqrf(rows, cols, A, rows, extra_arr);
+
+    } //end operator
+};
+
 
 int main(int argc, char** argv) {
 
     struct qrcp_wide_qp3 {
         std::vector<float> tau_vec;
-        void reserve(int64_t _m, int64_t _n) { tau_vec.resize(_n, 0.0); }
+        void reserve(int64_t rows, int64_t cols) { tau_vec.resize(cols, 0.0); }
         void free() { return; }
-        void operator()(int64_t _m, int64_t _n, float* A, int64_t _lda, int64_t* J) {
-            randblas_require( static_cast<int64_t>(tau_vec.size()) >= _n);
+        void operator()(int64_t rows, int64_t cols, float* A, int64_t _lda, int64_t* J) {
+            randblas_require( static_cast<int64_t>(tau_vec.size()) >= cols);
             float* tau = tau_vec.data();
-            lapack::geqp3(_m, _n, A, _lda, J, tau);
+            lapack::geqp3(rows, cols, A, _lda, J, tau);
         }
     };
 
+//
+// Riley's test problem
+    using T    = float;
+    int64_t mm = 8;
+    int64_t nn = 12;
+    std::vector<T> M0(mm*nn);
+    for (int64_t i = 0; i < nn; ++i) {
+        M0[i*mm + (i%mm)] = (T) (i+1);
+    }
+
+    RandBLAS::print_buff_to_stream(std::cout, blas::Layout::ColMajor, mm, nn, M0.data(), mm, "M0", 2);
+    
+    //tau = vec of floats, same to gqref init to 0, len cols
+    T* tau = new T[nn];
+    for (int i = 0; i < nn; i++) { tau[i] = 0.0;}
+
+    //call the setup_work function
+    setup_work<float> qrcp_wide{};
+    qrcp_wide.num_blocks = 3; //setting some variables just to test M0
+    qrcp_wide.block_size = 4;
+
+    RandLAPACK::QRBBRP<float, setup_work<float>> alg(qrcp_wide, true, 2,1.2); 
+
+    ///J is just array int 64 of all 0s num cols
+    int64_t* J = new int64_t[nn];
+    for (int i = 0; i < nn; i++) { J[i] = 0.0;}
+
+    RandBLAS::RNGState alg_state(99);
+    auto out_alg_state = alg.call(mm, nn, M0.data(), mm, J, tau, alg_state);
+
+//-----------------------------------------
+
+//----RILEY'S OG--------------------------
+/*
     int64_t M = 500;
     int64_t n = 50;
     int64_t b = 20;
@@ -48,4 +181,7 @@ int main(int argc, char** argv) {
 
     RandBLAS::RNGState alg_state(99);
     auto out_alg_state = QRBBRP.call(M, N, A, lda, J, tau, alg_state);
+*/
+    
+    
 }
