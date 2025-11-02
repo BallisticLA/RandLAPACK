d7336cb706f4aada7927f88ee4fa6d5a0706af4c
Riley Murray <rjmurr@sandia.gov>
2025-11-02 11:43:51 -0800

    use RandBLAS 1.1 API

--- demos/richol/demo_linsolve.cc
+++ demos/richol/demo_linsolve.cc
@@ -9,19 +9,17 @@
 #include <chrono>
 
 using RandBLAS::CSRMatrix;
-using RandBLAS::sparse_data::reserve_csr;
-using RandBLAS::sparse_data::reserve_coo;
 using RandBLAS::CSCMatrix;
 using RandBLAS::COOMatrix;
-using RandBLAS::sparse_data::conversions::coo_to_csr;
 
+using std::vector;
 using namespace richol::linops;
 
 
 template <typename T>
-std::vector<T> vector_from_matrix_market(std::string fn) {
+vector<T> vector_from_matrix_market(std::string fn) {
     int64_t n_rows, n_cols = 0;
-    std::vector<T> vals{};
+    vector<T> vals{};
     std::ifstream file_stream(fn);
     fast_matrix_market::read_matrix_market_array(
         file_stream, n_rows, n_cols, vals, fast_matrix_market::col_major
@@ -37,10 +35,10 @@ struct LaplacianPinv2 {
     CallableSpMat<SpMat>    L_callable;
     PrecondCallable &N_callable;
     bool verbose_pcg;
-    std::vector<double> work_B{};
-    std::vector<double> work_C{};
-    std::vector<double> work_seminorm{};
-    std::vector<double> times{};
+    vector<double> work_B{};
+    vector<double> work_C{};
+    vector<double> work_seminorm{};
+    vector<double> times{};
     double call_pcg_tol = 1e-10;
     int64_t max_iters = 100;
     using scalar_t = typename SpMat::scalar_t;
@@ -82,7 +80,7 @@ struct LaplacianPinv2 {
         if (n < (int64_t) L_callable.regs.size()) {
             L_callable.regs.resize(n, 0.0);
         }
-        std::vector<double> sn_log{};
+        vector<double> sn_log{};
         auto seminorm = [work_seminorm_, &sn_log](int64_t __n, int64_t __s, double* NR) {
             blas::copy(__n*__s, NR, 1, work_seminorm_, 1);
             double out = blas::nrm2(__n*__s, work_seminorm_, 1);
@@ -132,34 +130,33 @@ int main(int argc, char** argv) {
     if (argc > 1) {
         datadir = argv[1];
     }
-    auto A_unperm_coo = richol::from_matrix_market<T>(datadir + "/p_rgh_matrix_A.mtx");
-    blas::scal(A_unperm_coo.nnz, -1, A_unperm_coo.vals, 1);
+    auto A_coo = richol::from_matrix_market<T>(datadir + "/p_rgh_matrix_A.mtx");
+    blas::scal(A_coo.nnz, -1, A_coo.vals, 1);
     T reg = 0.0;
     bool offdiag_of_A_is_nonpos = true;
-    for (int64_t i = 0; i < A_unperm_coo.nnz; ++i) {
-        if (A_unperm_coo.rows[i] == A_unperm_coo.cols[i]) {
-            A_unperm_coo.vals[i] += reg;
+    for (int64_t i = 0; i < A_coo.nnz; ++i) {
+        if (A_coo.rows[i] == A_coo.cols[i]) {
+            A_coo.vals[i] += reg;
         } else if (offdiag_of_A_is_nonpos) {
-            offdiag_of_A_is_nonpos = A_unperm_coo.vals[i] <= 0;
+            offdiag_of_A_is_nonpos = A_coo.vals[i] <= 0;
         }
     }
-    int64_t n0 = A_unperm_coo.n_rows;
+    int64_t n0 = A_coo.n_rows;
 
     // Step 2. Compute AMD ordering
-    CSRMatrix<T> A_unperm_csr(n0, n0);
-    coo_to_csr(A_unperm_coo, A_unperm_csr);
-    std::vector<int64_t> perm(n0, 0);
+    auto A_unperm_csr = A_coo.as_owning_csr();
+    vector<int64_t> perm(n0, 0);
     std::iota(perm.data(), perm.data() + n0, 0);
-    //richol::amd_permutation(A_unperm_csr, perm);
-    CSRMatrix<T> A_csr(n0, n0);
-    richol::permuted(A_unperm_csr, perm, A_csr);
+    // richol::amd_permutation(A_unperm_csr, perm);
+    A_coo.symperm_inplace(perm.data());
+    auto A_csr = A_coo.as_owning_csr();
 
     std::cout << std::endl << "SDD is M-matrix : " << offdiag_of_A_is_nonpos << std::endl;
 
     // Step 3. Compute the Gremban expansion
-    std::vector<spvec> A_csrlike_triu(n0);
+    vector<spvec> A_csrlike_triu(n0);
     richol::sym_as_upper_tri_from_csr(n0, A_csr.rowptr, A_csr.colidxs, A_csr.vals, A_csrlike_triu);
-    std::vector<spvec> G_csrlike_triu{};
+    vector<spvec> G_csrlike_triu{};
     if (offdiag_of_A_is_nonpos) {
         G_csrlike_triu = A_csrlike_triu;
     } else {
@@ -173,7 +170,7 @@ int main(int argc, char** argv) {
 
     // Step 4. Compute the incomplete Cholesky decomposition
     RandBLAS::RNGState s_richol(0);
-    std::vector<spvec> C_csrlike_lower{};
+    vector<spvec> C_csrlike_lower{};
     bool diag_adjust = true;
     int64_t rank = richol::clb21_rand_cholesky(G_csrlike_triu, C_csrlike_lower, s_richol, diag_adjust, (T)0.0);
     std::cout << "dim : " << n << std::endl;
@@ -186,7 +183,7 @@ int main(int argc, char** argv) {
     for (const auto &row : C_csrlike_lower)
         nnz_C += static_cast<int64_t>(row.size());
     CSRMatrix<T> C_lower(n, n);
-    reserve_csr(nnz_C, C_lower);
+    C_lower.reserve(nnz_C);
     csr_from_csrlike(C_csrlike_lower, C_lower.rowptr, C_lower.colidxs, C_lower.vals);
     trsm_matrix_validation(C_lower, Uplo::Lower, Diag::NonUnit, 3);
     CallableChoSolve<decltype(C_lower)> invCCt_callable{&C_lower, n};
@@ -205,7 +202,7 @@ int main(int argc, char** argv) {
     //
     //      auto [M_csrlike, P_csrlike] = richol::split_and_sym_sdd(A_csrlike_triu);
     //
-    std::vector<spvec> G_csrlike;
+    vector<spvec> G_csrlike;
     if (offdiag_of_A_is_nonpos) {
         G_csrlike = G_csrlike_triu;
         int64_t row_ind = 0;
@@ -224,7 +221,7 @@ int main(int argc, char** argv) {
     for (const auto &row : G_csrlike)
         nnz_G += static_cast<int64_t>(row.size());
     CSRMatrix<T> G(n, n);
-    reserve_csr(nnz_G, G);
+    G.reserve(nnz_G);
     csr_from_csrlike(G_csrlike, G.rowptr, G.colidxs, G.vals);
     CallableSpMat<decltype(G)> G_callable{&G, n};
     G_callable.project_out = false;
@@ -234,13 +231,13 @@ int main(int argc, char** argv) {
 
 
     // Step 7. Setup PCG.
-    // std::vector<T> b(n);
+    // vector<T> b(n);
     // RandBLAS::RNGState state2(1997);
     // RandBLAS::DenseDist D(n, 1);
     // RandBLAS::fill_dense(D, b.data(), state2);
     int64_t block_size = 1;
-    std::vector<T> b0 = vector_from_matrix_market<T>(datadir + "/p_rgh_source_b.mtx");
-    std::vector<T> b(n);
+    vector<T> b0 = vector_from_matrix_market<T>(datadir + "/p_rgh_source_b.mtx");
+    vector<T> b(n);
     for (int64_t i = 0; i < n; ++i) {
         b[perm[i]] = -b0[i];
     }
@@ -249,9 +246,9 @@ int main(int argc, char** argv) {
     // RandBLAS::DenseDist D(n, block_size);
     // RandBLAS::fill_dense(D, b.data(), state2);
     // blas::scal(n*block_size, (T)1.0 / blas::nrm2(n, b.data(), 1), b.data(), 1);
-    // std::vector<T> x(block_size * n, (T)0.0);
-    std::vector<T> x0 = vector_from_matrix_market<T>(datadir + "/p_rgh_psi_initial_x^{n}.mtx");
-    std::vector<T> x(n);
+    // vector<T> x(block_size * n, (T)0.0);
+    vector<T> x0 = vector_from_matrix_market<T>(datadir + "/p_rgh_psi_initial_x^{n}.mtx");
+    vector<T> x(n);
     for (int64_t i = 0; i < n; ++i) {
         x[perm[i]] = x0[i];
     }
@@ -259,7 +256,7 @@ int main(int argc, char** argv) {
     struct trivial_precond {
         int64_t num_ops = 1;
         const int64_t dim;
-        std::vector<double> times{};
+        vector<double> times{};
         trivial_precond(int64_t _n) : dim(_n), times(4,(T)0.0) { }
         void operator()(blas::Layout ell, int64_t _n, T alpha, T* const _B, int64_t _ldb, T beta, T* _C, int64_t _ldc) {
             randblas_require(ell == blas::Layout::ColMajor);
