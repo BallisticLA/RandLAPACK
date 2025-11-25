bae7b6441e93f51a2894bb8b2aca83a67ec88187
mmelnich <m.y.melnich@gmail.com>
2025-11-25 15:58:08 -0500

    Benchmark build fix

--- CMake/rl_config.cmake
+++ CMake/rl_config.cmake
@@ -1,11 +1,11 @@
 
 configure_file(CMake/RandLAPACKConfig.cmake.in
-    ${CMAKE_INSTALL_LIBDIR}/cmake/RandLAPACKConfig.cmake @ONLY)
+    ${CMAKE_INSTALL_LIBDIR}/cmake/RandLAPACK/RandLAPACKConfig.cmake @ONLY)
 
 configure_file(CMake/RandLAPACKConfigVersion.cmake.in
-    ${CMAKE_INSTALL_LIBDIR}/cmake/RandLAPACKConfigVersion.cmake @ONLY)
+    ${CMAKE_INSTALL_LIBDIR}/cmake/RandLAPACK/RandLAPACKConfigVersion.cmake @ONLY)
 
 install(FILES
-    ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/RandLAPACKConfig.cmake
-    ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/RandLAPACKConfigVersion.cmake
-    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake)
+    ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/RandLAPACK/RandLAPACKConfig.cmake
+    ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/RandLAPACK/RandLAPACKConfigVersion.cmake
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/RandLAPACK)
