e2038cf534da7a273442d682acd346fd3da6e41c
mmelnich <m.y.melnich@gmail.com>
2026-03-16 16:31:01 -0400

    Moving functions

--- benchmark/bench_CQRRT_linops/generate_dense_spd.cc
+++
@@ -1,8 +0,0 @@
-#if defined(__APPLE__)
-int main() {return 0;}
-#else
-#include "RandLAPACK/testing/rl_generate_dense_spd.hh"
-int main(int argc, char* argv[]) {
-    return RandLAPACK::testing::run_generate_dense_spd(argc, argv);
-}
-#endif
--- benchmark/bench_CQRRT_linops/generate_sparse_spd.cc
+++
@@ -1,8 +0,0 @@
-#if defined(__APPLE__)
-int main() {return 0;}
-#else
-#include "RandLAPACK/testing/rl_generate_sparse_spd.hh"
-int main(int argc, char* argv[]) {
-    return RandLAPACK::testing::run_generate_sparse_spd(argc, argv);
-}
-#endif
